5bfcd9322b78e6239a7eeb0b0aa6d8f2a17751bc
Michael <m.langguth@fz-juelich.de>
2021-11-17 14:08:58 +0100

    Update batch-script template for training.

--- downscaling_gan/batch_scripts/train_downscaling_GAN_template.sh
+++ downscaling_gan/batch_scripts/train_downscaling_GAN_template.sh
@@ -6,33 +6,27 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --output=train_downscaling_GAN-out.%j
 #SBATCH --error=train_downscaling_GAN-err.%j
-#SBATCH --time=00:20:00
+#SBATCH --time=24:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --partition=batch
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=m.langguth@fz-juelich.de
-##jutil env activate -p cjjsc42
 
 ######### Template identifier (don't remove) #########
 echo "Do not run the template scripts"
 exit 99
 ######### Template identifier (don't remove) #########
 
-# Name of virtual environment 
-#VIRT_ENV_NAME="my_venv"
+# set some paths
+WORK_DIR=`pwd`
+BASE_DIR=$(dirname "$WORK_DIR")
+# Name of virtual environment
+VIRT_ENV_NAME="my_venv"
+# Name of container image (must be available in working directory)
+CONTAINER_IMG="${WORK_DIR}/tensorflow_21.09-tf1-py3.sif"
 
-# Loading mouldes
-source ../env_setup/modules_train.sh
-# Activate virtual environment if needed (and possible)
-#if [ -z ${VIRTUAL_ENV} ]; then
-#   if [[ -f ../${VIRT_ENV_NAME}/bin/activate ]]; then
-#      echo "Activating virtual environment..."
-#      source ../${VIRT_ENV_NAME}/bin/activate
-#   else 
-#      echo "ERROR: Requested virtual environment ${VIRT_ENV_NAME} not found..."
-#      exit 1
-#   fi
-#fi
+# clean-up modules to avoid conflicts between host and container settings
+module purge
 
 # variables for settings
 application="mchrzc"
@@ -41,12 +35,10 @@ dest_file_wgt=""
 dest_file_log=""
 
 # Please uncomment the following CUDA configuration
-#CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=1
 
 # run training
-srun python ../dsrnngan/main_train.py --application=${application} --data_file=${data_in} \
+srun --mpi=pspmix --cpu-bind=none \
+singularity exec --nv ${CONTAINER_IMG} ./wrapper_container.sh ${VIRT_ENV_NAME}  python3 ../dsrnngan/main_train.py \
+                                      --application=${application} --data_file=${data_in} \
                                       --save_weights_root=${dest_file_wgt} --log_path ${dest_file_log} 
-
-
-
- 
