3e96ed3b53ed0f3702bef2c2a0b829a0a1246e97
Michael <m.langguth@fz-juelich.de>
2021-11-18 14:10:17 +0100

    Improve verbosity of install_venv_container.sh.

--- downscaling_gan/env_setup/install_venv_container.sh
+++ downscaling_gan/env_setup/install_venv_container.sh
@@ -1,42 +1,43 @@
 #!/usr/bin/env bash
 #
 # __authors__ = Bing Gong, Michael Langguth
-# __date__  = '2021_10_28'
-# __last_update__  = '2021_10_28' by Michael Langguth
+# __date__  = '2021-10-28'
+# __last_update__  = '2021-11-18' by Michael Langguth
 #
 # **************** Description ****************
 # This auxiliary script sets up the virtual environment within a singularity container.
 # **************** Description ****************
 
+SCR_INSTALL="%install_venv_container.sh: "            # this script
 # set some basic variables
 BASE_DIR=$(pwd)
 VENV_DIR=$1
-VENV_NAME="$(basename "${VENV_BASE}")"
+VENV_NAME="$(basename "${VENV_DIR}")"
 VENV_BASE="$(dirname "${VENV_DIR}")"
 
 # sanity checks
 # check if we are running in a container
 if [ -z "${SINGULARITY_NAME}" ]; then
-  echo "ERROR: install_venv_container.sh must be called within a running singularity container."
+  echo "${SCR_INSTALL}ERROR: install_venv_container.sh must be called within a running singularity container."
   return
 fi
 
 # check if directory to virtual environment is parsed
 if [ -z "$1" ]; then
-  echo "ERROR: Provide a name to set up the virtual environment."
-  return
+  echo "${SCR_INSTALL}ERROR: Provide a name to set up the virtual environment."
+  exit
 fi
 
 # check if virtual environment is not already existing
 if [ -d "$1" ]; then
-  echo "ERROR: Target directory of virtual environment ${1} already exists. Choose another directory path."
-  return
+  echo "${SCR_INSTALL}ERROR: Target directory of virtual environment ${1} already exists. Choose another directory path."
+  exit
 fi
 
 # check for requirement-file
 if [ ! -f "${BASE_DIR}/requirements_container.txt" ]; then
-  echo "ERROR: Cannot find requirement-file ${BASE_DIR}/requirements_container.txt to set up virtual environment."
-  return
+  echo "${SCR_INSTALL}ERROR: Cannot find requirement-file ${BASE_DIR}/requirements_container.txt to set up virtual environment."
+  exit
 fi
 
 # remove dependancies from system packages
@@ -46,7 +47,7 @@ export PYTHONPATH=
 if ! [[ -d "${VENV_BASE}" ]]; then
   mkdir "${VENV_BASE}"
   # Install virtualenv in this directory
-  echo "Installing virtualenv under ${VENV_BASE}..."
+  echo "${SCR_INSTALL}Installing virtualenv under ${VENV_BASE}..."
   pip install --target="${VENV_BASE}/" virtualenv
   # Change into the base-directory of virtual environments...
   cd "${VENV_BASE}" || return
@@ -54,13 +55,16 @@ else
   # Change into the base-directory of virtual environments...
   cd "${VENV_BASE}" || return
   if ! python -m virtualenv --version >/dev/null; then
-    echo "ERROR: Base directory for virtual environment exists, but virtualenv-module is unavailable."
+    echo "${SCR_INSTALL}ERROR: Base directory for virtual environment exists, but virtualenv-module is unavailable."
+    exit
   fi
+  echo "${SCR_INSTALL}Virtualenv is already installed."
 fi
 # ... and set-up virtual environment therein
-python -m virtualenv -p /usr/bin/python "${VENV_NAME}"
+#python -m virtualenv -p /usr/bin/python "${VENV_NAME}"
+python -m virtualenv "${VENV_NAME}"
 # Activate virtual environment and install required packages
-echo "Actiavting virtual environment ${VENV_NAME} to install required Python modules..."
+echo "${SCR_INSTALL}Activating virtual environment ${VENV_NAME} to install required Python modules..."
 source "${VENV_DIR}/bin/activate"
 # set PYTHONPATH and install packages
 export PYTHONPATH="/usr/local/lib/python3.8/dist-packages/"
