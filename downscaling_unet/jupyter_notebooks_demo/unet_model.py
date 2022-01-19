c7536e92919eabeab9b59b5e3ad613f6e51d059a
Michael <m.langguth@fz-juelich.de>
2022-01-19 17:45:46 +0100

    Add Python-script to build the u-net used for the downscaling application.

---
+++ downscaling_unet/jupyter_notebooks_demo/unet_model.py
@@ -0,0 +1,95 @@
+# import tensorflow and required stuff from Keras API
+import tensorflow as tf
+
+# all the layers used for U-net
+from tensorflow.keras.layers import (Activation, BatchNormalization, Concatenate, Conv2D,
+                                     Conv2DTranspose, Input, MaxPool2D
+)
+from tensorflow.keras.models import Model
+
+# building blocks for Unet
+
+def conv_block(inputs, num_filters: int, kernel: tuple = (3,3), padding: str = "same",
+               activation: str = "relu", kernel_init: str = "he_normal", l_batch_normalization: bool = True):
+    """
+    A convolutional layer with optional batch normalization
+    :param inputs: the input data with dimensions nx, ny and nc
+    :param num_filters: number of filters (output channel dimension)
+    :param kernel: tuple indictating kernel size
+    :param padding: technique for padding (e.g. "same" or "valid")
+    :param activation: activation fuction for neurons (e.g. "relu")
+    :param kernel_init: initialization technique (e.g. "he_normal" or "glorot_uniform")
+    """
+    x = Conv2D(num_filters, kernel, padding=padding, kernel_initializer=kernel_init)(inputs)
+    if l_batch_normalization:
+        x = BatchNormalization()(x)
+    x = Activation(activation)(x)
+
+    return x
+
+
+def conv_block_n(inputs, num_filters, n=2, kernel=(3, 3), padding="same", activation="relu",
+                 kernel_init="he_normal", l_batch_normalization=True):
+    """
+    Sequential application of two convolutional layers (using conv_block).
+    """
+
+    x = conv_block(inputs, num_filters, kernel, padding, activation,
+                   kernel_init, l_batch_normalization)
+    for i in np.arange(n - 1):
+        x = conv_block(x, num_filters, kernel, padding, activation,
+                       kernel_init, l_batch_normalization)
+
+    return x
+
+
+def encoder_block(inputs, num_filters, kernel_maxpool: tuple = (2, 2), l_large: bool = True):
+    """
+    One complete encoder-block used in U-net
+    """
+    if l_large:
+        x = conv_block_n(inputs, num_filters, n=2)
+    else:
+        x = conv_block(inputs, num_filters)
+
+    p = MaxPool2D(kernel_maxpool)(x)
+
+    return x, p
+
+def decoder_block(inputs, skip_features, num_filters, kernel: tuple=(3,3), strides_up: int=2, padding: str= "same",
+                  activation="relu", kernel_init="he_normal", l_batch_normalization: bool=True):
+    """
+    One complete decoder block used in U-net (reverting the encoder)
+    """
+    x = Conv2DTranspose(num_filters, (strides_up, strides_up), strides=strides_up, padding="same")(inputs)
+    x = Concatenate()([x, skip_features])
+    x = conv_block_n(x, num_filters, 2, kernel, padding, activation, kernel_init, l_batch_normalization)
+
+    return x
+
+# The particular U-net
+def build_unet(input_shape, channels_start=56, z_branch=False):
+    inputs = Input(input_shape)
+
+    """ encoder """
+    s1, e1 = encoder_block(inputs, channels_start, l_large=True)
+    s2, e2 = encoder_block(e1, channels_start * 2, l_large=False)
+    s3, e3 = encoder_block(e2, channels_start * 4, l_large=False)
+
+    """ bridge encoder <-> decoder """
+    b1 = conv_block(e3, channels_start * 8)
+
+    """ decoder """
+    d1 = decoder_block(b1, s3, channels_start * 4)
+    d2 = decoder_block(d1, s2, channels_start * 2)
+    d3 = decoder_block(d2, s1, channels_start)
+
+    output_temp = Conv2D(1, (1, 1), kernel_initializer="he_normal", name="output_temp")(d3)
+    if z_branch:
+        output_z = Conv2D(1, (1, 1), kernel_initializer="he_normal", name="output_z")(d3)
+
+        model = Model(inputs, [output_temp, output_z], name="t2m_downscaling_unet_with_z")
+    else:
+        model = Model(inputs, output_temp, name="t2m_downscaling_unet")
+
+    return model
