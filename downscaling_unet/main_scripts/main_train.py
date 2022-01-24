ab064688e21344a47bc1a5a92e291fcfc1dd652e
Michael <m.langguth@fz-juelich.de>
2022-01-24 17:24:03 +0100

    Further adapt main_Train.py to track benchmark parameters systematically.

--- downscaling_unet/main_scripts/main_train.py
+++ downscaling_unet/main_scripts/main_train.py
@@ -1,45 +1,50 @@
 __author__ = "Michael Langguth"
 __email__ = "m.langguth@fz-juelich.de"
 __date__ = "2022-01-20"
-__update__ = "2022-01-22"
+__update__ = "2022-01-24"
 
 import os, sys
 import argparse
-import time
-import json
 from timeit import default_timer as timer
 import tensorflow.keras as keras
 from tensorflow.keras.optimizers import Adam
 import tensorflow.keras.utils as ku
-from handle_data_unet import *
+from handle_data_unet import HandleUnetData
 from unet_model import build_unet, get_lr_scheduler
-import numpy as np
+from benchmark_utils import BenchmarkCSV, get_training_time_dict
 
 
 def main(parser_args):
 
+    # start timing
+    t0 = timer()
+
     method = main.__name__
 
+    # parse arguments
     datadir = parser_args.input_dir
     outdir = parser_args.output_dir
 
+    z_branch = not parser_args.no_z_branch
     hour = parser_args.hour
     nepochs = parser_args.nepochs
 
-    # Read and normalize data for training
+    # initialize benchmarking object
+    bm_obj = BenchmarkCSV(os.getcwd(), "benchmark_training.csv")
+    # read and normalize data for training
     data_obj = HandleUnetData(datadir)
 
     int_data, tart_data, opt_norm = data_obj.normalize("train", daytime=hour)
     inv_data, tarv_data = data_obj.normalize("val", daytime=hour, opt_norm=opt_norm)
 
+    # get dictionary for tracking benchmark parameters
     benchmark_dict = data_obj.timing
 
-    print(benchmark_dict)
-    print(data_obj.data_info["memory_datasets"])
-    print(data_obj.data_info["nsamples"])
+    # some information on training data
+    nsamples = int_data.shape[0]
+    shape_in = int_data.shape[1:]
 
-    shape_in = (96, 128, 3)
-
+    # visualize model architecture on login-node
     if "login" in data_obj.host:
         unet_model = build_unet(shape_in, z_branch=True)
         ku.plot_model(unet_model, to_file=os.path.join(outdir, "unet_downscaling_model.png"), show_shapes=True)
@@ -50,24 +55,23 @@ def main(parser_args):
             self.epoch_times = []
 
         def on_epoch_begin(self, epoch, logs={}):
-            self.epoch_time_start = time.time()
+            self.epoch_time_start = timer()
 
         def on_epoch_end(self, epoch, logs={}):
-            self.epoch_times.append(time.time() - self.epoch_time_start)
+            self.epoch_times.append(timer() - self.epoch_time_start)
 
-    z_branch = True  # flag if additionally training on surface elevation is performed
-
+    # create callbacks for scheduling learning rate and for timing training process
     lr_scheduler, time_tracker = get_lr_scheduler(), TimeHistory()
-    # create callbacks
     callback_list = [lr_scheduler, time_tracker]
 
     # build, compile and train the model
     unet_model = build_unet(shape_in, z_branch=z_branch)
 
     if z_branch:
+        print("%{0}: Start training with optimization on surface topography (with z_branch).".format(method))
         unet_model.compile(optimizer=Adam(learning_rate=5*10**(-4)),
-                       loss={"output_temp": "mae", "output_z": "mae"},
-                       loss_weights={"output_temp": 1.0, "output_z": 1.0})
+                           loss={"output_temp": "mae", "output_z": "mae"},
+                           loss_weights={"output_temp": 1.0, "output_z": 1.0})
 
         history = unet_model.fit(x=int_data.values, y={"output_temp": tart_data.isel(variable=0).values,
                                                        "output_z": tart_data.isel(variable=1).values},
@@ -76,37 +80,42 @@ def main(parser_args):
                                                                     "output_z": tarv_data.isel(variable=1).values}), 
                                  verbose=2)
     else:
+        print("%{0}: Start training without optimization on surface topography (with z_branch).".format(method))
         unet_model.compile(optimizer=Adam(learning_rate=5*10**(-4)), loss="mae")
-
 
         history = unet_model.fit(x=int_data.values, y=tart_data.isel(variable=0).values, batch_size=32,
                                  epochs=nepochs, callbacks=callback_list,
                                  validation_data=(inv_data.values, tarv_data.isel(variable=0).values), 
                                  verbose=2)
 
-    epoch_times = time_tracker.epoch_times
-
-    training_times = {"training" : {"total_training_time": np.sum(epoch_times), "avg_epoch_time": np.mean(epoch_times),
-                      "min_training_time": np.amin(epoch_times), "max_training_time": np.amax(epoch_times[1:]),
-                      "first_epoch_time": epoch_times[0], "number_samples": np.shape(int_data)[0]}}
+    # get some parameters from tracked training times and put to dictionary
+    training_times = get_training_time_dict(time_tracker.epoch_times, nsamples*nepochs)
     benchmark_dict = {**benchmark_dict, **training_times}
-
-    print(history.history["output_temp_loss"][-1])
-    print(history.history["val_output_temp_loss"][-1])
-
-    print("Total training time: {0:.2f}s".format(np.sum(epoch_times)))
-    print("Max. time per epoch: {0:.4f}s, min. time per epoch: {1:.4f}s".format(np.amax(epoch_times),
-                                                                                np.amin(epoch_times)))
+    # also track losses
+    benchmark_dict["final training loss"] = history.history["output_temp_loss"][-1]
+    benchmark_dict["final validation loss"] = history.history["val_output_temp_loss"][-1]
 
     # save trained model
-    time0_save = timer()
-    unet_model.save(os.path.join(outdir, "trained_downscaling_unet_t2m_hour{0:0d}.h5".format(hour)), save_format="h5")
-    save_time = timer() - time0_save
-    benchmark_dict = {**benchmark_dict, "save_time": save_time}
-    print("%{0}: Saving trained model to '{1}' took {2:.2f}s.".format(method, outdir, save_time))
+    model_name =  "trained_downscaling_unet_t2m_hour{0:0d}_exp{1:d}".format(hour, benchmark_dict["Experiment number"])
+    print("%{0}: Save trained model '{1}' to '{2}'".format(method, model_name, outdir))
+    t0_save = timer()
+    unet_model.save(os.path.join(outdir, "{0}.h5".format(model_name)), save_format="h5")
+    benchmark_dict = {**benchmark_dict, "saving model time": timer() - t0_save}
 
-    with open(os.path.join(outdir, "benchmark_times.json"), "w") as f:
-        json.dump(benchmark_dict, f)
+    # finally, track total runtime...
+    benchmark_dict["total runtime"] = timer() - t0
+    # ... and save CSV-file with tracked data on disk
+    bm_obj.populate_csv_from_dict(benchmark_dict)
+
+    js_file = os.path.join(os.getcwd(), "data_info.json")
+    if not os.path.isfile(js_file):
+        data_mem = data_obj.data_info["memory_datasets"]
+        data_info = {"training data size": data_mem["train"], "validation data size": data_mem["val"],
+                     "nsamples": nsamples, "shape_samples": shape_in}
+
+        with open(js_file, "w") as jsf:
+            jsf.dump(data_info)
+
 
 
 if __name__ == "__main__":
@@ -119,6 +128,9 @@ if __name__ == "__main__":
                         help="Number of epochs for training.")
     parser.add_argument("--hour", "-hour", dest="hour", type=int, default=12,
                         help="Daytime hour for which model will be trained.")
+    parser.add_argument("--no_z_branch", "-no_z", dest="no_z_branch", default=False, action="store_true",
+                        help="Flag if U-net is optimzed on additional output branch for topography" +
+                             "(see Sha et al., 2020)")
 
     args = parser.parse_args()
     main(args)
