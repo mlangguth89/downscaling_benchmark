a7cf0b00ca9f423e27768f00e2db8b4005e14c3d
Michael <m.langguth@fz-juelich.de>
2022-01-31 22:54:21 +0100

    Furth updates and generalization to handle_data_class.py.

--- downscaling_unet/handle_data/handle_data_class.py
+++ downscaling_unet/handle_data/handle_data_class.py
@@ -1,7 +1,7 @@
 __author__ = "Michael Langguth"
 __email__ = "m.langguth@fz-juelich.de"
 __date__ = "2022-01-20"
-__update__ = "2022-01-22"
+__update__ = "2022-01-31"
 
 import os, sys
 import socket
@@ -16,32 +16,31 @@ class HandleDataClass(object):
         Initialize Input data object by reading data from netCDF-files
         :param datadir: the directory from where netCDF-files are located (or should be located if downloaded)
         :param application: name of application (must coincide with name in s3-bucket)
-        :param fname_base: prefix for naming the netCDF-files on disk
+        :param query: query string which can be used to load data from the s3-bucket of the application
+        :param purpose: optional name to indicate the purpose of queried data (used as key for the data-dictionary)
         """
         method = HandleDataClass.__init__.__name__
 
         self.host = os.getenv("HOSTNAME") if os.getenv("HOSTNAME") is not None else "unknown"
         purpose = query if purpose is None else purpose
         self.application = application
+        self.datadir = datadir
         if not os.path.isdir(datadir):
             os.makedirs(datadir)
+        self.ldownload_last = None
 
-        ds = self.handle_data_req(query, purpose)
-        self.ldownload = self.set_download_flag()
-        self.datafile = os.path.join(datadir, "{0}_{1}.nc".format(application, self.purpose))
-        t0_load = timer()
-        ds = self.get_data()
-        load_time = t0_load - timer()
-        if self.ldownload:
-            print("%{0}: Downloading the data took {0:.2f}s.".format(method, load_time))
-            _ = HandleDataClass.ds_to_netcdf(ds, self.datafile)
-        else:
-            self.timing = {"loading_times": purpose: load_time}
-            self.data = {purpose: ds}
-        self.data_info = {"memory_datasets": {self.purpose: ds.nbytes}}
+        self.data, self.timing, self.data_info = self.handle_data_req(query, purpose)
 
     def handle_data_req(self, query: str, purpose):
-        datafile = os.path.join(self.datadir, "{0}_{1}.nc".format(self.application))
+        """
+        Handles a data-query by parsing it to the get_data function
+        :param query: the query-string to submit to the climetlab-API of the application
+        :param purpose: the name/purpose of the retireved data (used to append the data-dictionary)
+        :return: the xr.Dataset retireved from get_data and dictionaries for the loading time and the memory consumption
+        """
+        method = HandleDataClass.handle_data_req.__name__
+
+        datafile = os.path.join(self.datadir, "{0}_{1}.nc".format(self.application, purpose))
         self.ldownload_last = HandleDataClass.set_download_flag(datafile)
         # time data retrieval
         t0_load = timer()
@@ -49,23 +48,27 @@ class HandleDataClass(object):
         load_time = timer() - t0_load
         if self.ldownload_last:
             print("%{0}: Downloading took {1:.2f}s.".format(method, load_time))
-            _ = HandleDataClass.ds_to_netcdf(ds, self.datafile)
+            _ = HandleDataClass.ds_to_netcdf(ds, datafile)
 
+        data = {purpose: ds},
+        timing = {"loading_times": {purpose: load_time}}
+        data_info = {"memory_datasets": {purpose: ds.nbytes}}
 
-        return ds
-
-            self.timing = {"loading_times": purpose: load_time}
-            self.data = {purpose: ds}
-        self.data_info = {"memory_datasets": {self.purpose: ds.nbytes}}
+        return data, timing, data_info
 
     def append_data(self, query: str, purpose: str = None):
+        """
+        Appends data-dictionary of the class and also tracks basic benchmark parameters
+        :param query: the query-string to submit to the climetlab-API of the application
+        :param purpose: the name/purpose of the retireved data (used to append the data-dictionary)
+        :return: appended self.data-dictionary with {purpose: xr.Dataset}
+        """
+        purpose = query if purpose is None else purpose
+        ds_app, timing_app, data_info_app = self.handle_data_req(query, purpose)
 
-        self.purpose = query if purpose is None else purpose
-        self.ldownload = self.set_download_flag()
-        self.datafile = os.path.join(datadir, "{0}_{1}.nc".format(application, self.purpose))
-        t0_load = timer()
-        ds = self.get_data()
-
+        self.data.update(ds_app)
+        self.timing.update(timing_app)
+        self.data_info.update(data_info_app)
 
     def set_download_flag(self, datafile):
         """
