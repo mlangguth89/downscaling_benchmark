21da9fa58af12214714d44f4f9a355dce3d6d284
masak1112 <gongbing1112@gmail.com>
2022-03-08 20:54:13 +0100

    add load opt_norm json file for normalizing test dataset

--- downscaling_unet/main_scripts/main_postprocess.py
+++ downscaling_unet/main_scripts/main_postprocess.py
@@ -4,7 +4,7 @@ from timeit import default_timer as timer
 import tensorflow.keras as keras
 import xarray as xr
 from handle_data_unet import HandleUnetData
-
+import json
 class TimeHistory(keras.callbacks.Callback):
     def on_predict_begin(self,logs={}):
         self.batch_times = []
@@ -16,9 +16,12 @@ class TimeHistory(keras.callbacks.Callback):
         self.batch_times.append(timer() - self.batch_time_start)
 
     def on_predict_end(self,logs={}):
-        print("the time for inference per batch",self.batch_times)
+        print("The time for inference per batch",self.batch_times)
+        print("The average of inference time per batch is",sum(self.batch_times)/len(self.batch_times))
+
 
 def main(parser_args):
+     
 
     # start timing
     t0 = timer()
@@ -31,14 +34,20 @@ def main(parser_args):
     input_dir = parser_args.input_dir
     out_dir = parser_args.output_dir
     batch_size = args.batch_size
-    hour = args.hour 
+    opt_norm = args.opt_norm
+    
+    #load the json file for normalizing the test dataset
+    with open(opt_norm) as jfl:
+        opt_norm_js = json.load(jfl)
     
     #reconstruct the model
     model_recon = keras.models.load_model(model_path)
 
     #obtain the test dataset
-    data_obj = HandleUnetData(input_dir, "test", purpose="test")
-    int_data, tart_data, opt_norm = data_obj.normalize("test", daytime=hour)
+    data_obj = HandleUnetData(input_dir, "test", purpose="test_aug")
+    int_data, tart_data = data_obj.normalize("test_aug",  daytime=None, opt_norm=opt_norm_js)
+
+    
     time_tracker = TimeHistory()
     callback_list = [time_tracker]
     
@@ -60,8 +69,9 @@ if __name__ == "__main__":
                         help = "Batch size during model training.")
     parser.add_argument("--job_id", "-id", dest="job_id", type=int, help="Job-id from Slurm.")
     parser.add_argument("--hour", "-hour", dest="hour", type=int, default=12,
-                        help="Daytime hour for which model will be trained.")
-
+                        help="Daytime hour for which model will be trained.") 
+    parser.add_argument("--opt_norm_path", "-opt_norm", dest="opt_norm", type=str, default=None,
+                        help="opt_norm_path json file where you obtained from normalizing training dataset, will used for noramlizing test dataset")
     args = parser.parse_args()
     main(args)
 
