0f09dd89fe2ef88505e44abe7d5a2f7e78270d30
Michael <m.langguth@fz-juelich.de>
2022-04-30 13:26:49 +0200

    Set-up prepare_worker in preprocess_data_era5_to_ifs.py.

--- downscaling_unet/preprocess/preprocess_data_era5_to_ifs.py
+++ downscaling_unet/preprocess/preprocess_data_era5_to_ifs.py
@@ -1,7 +1,7 @@
 __author__ = "Michael Langguth"
 __email__ = "m.langguth@fz-juelich.de"
-__date__ = "2022-03-16"
-__update__ = "2022-03-18"
+__date__ = "2022-04-22"
+__update__ = "2022-04-29"
 
 # doc-string
 """
@@ -9,7 +9,7 @@ Main script to preprocess ERA5 data (provided on a 0.3°-grid) for first real do
 The target of the downscaling will be IFS HRES data on a 0.1°-grid as in preprocess_data_unet_tier1.py.
 Contrarily to the previous, simplified approach, no slicing regarding daytime and season will be performed. 
 Thus, hourly input data are produced from the ERA5-dataset. For the output, hourly target data, 
-IFS forecasts with lead time 3 to 14 hours is used. 
+IFS forecasts with lead time 6 to 17 hours is used. 
 """
 # doc-string
 
@@ -25,38 +25,41 @@ from collections import OrderedDict
 from other_utils import to_list
 from pystager_utils import PyStager
 from abstract_preprocess import Abstract_Preprocessing
-from preprocess_data_unet_tier1 import Preprocess_Unet_Tier1
+from preprocess_data_unet_tier1 import Preprocess_Unet_Tier1, CDOGridDes
 from tools_utils import CDO, NCRENAME, NCAP2, NCKS, NCEA
 
 number = Union[float, int]
 num_or_List = Union[number, List[number]]
 list_or_tuple = Union[List, tuple]
 
-class Preprocess_ERA5_to_IFS(Abstract_Preprocessing):
+
+class PreprocessERA5toIFS(AbstractPreprocessing):
 
     # expected key of grid description files
     expected_keys_gdes = ["gridtype", "xsize", "ysize", "xfirst", "xinc", "yfirst", "yinc"]
 
-    def __init__(self, source_dir: str, output_dir: str, grid_des_tar: str, downscaling_fac: int = None):
+    def __init__(self, source_dir_era5: str, source_dir_ifs, output_dir: str, grid_des_tar: str, predictors : dict,
+                 predictands: dict, downscaling_fac: int = 8):
         """
         Initialize class for tier-1 downscaling dataset.
         """
-        super().__init__("preprocess_ERA5_to_IFS", source_dir, output_dir)
+        super().__init__("preprocess_ERA5_to_IFS", source_dir_era5, source_dir_ifs, predictors, predictands, output_dir)
 
         if not os.path.isfile(grid_des_tar):
             raise FileNotFoundError("Preprocess_Unet_Tier1: Could not find target grid description file '{0}'"
                                     .format(grid_des_tar))
         self.grid_des_tar = grid_des_tar
         self.my_rank = None                     # to be set in __call__
+        self.downscaling_fac = downscaling_fac
 
-    def __call__(self, years: List, season: str, jobname: str = "dummy", **kwargs):
+    def prepare_worker(self, years: List, season: str, **kwargs):
         """
-        Run preprocessing.
+        Prepare workers for preprocessing.
         :param years: List of years to be processed.
-        :param months: List of months to be processed.
-        :param jobname: jobname-identifier for PyStager
+        :param season: Season-string to be processed.
+        :param kwargs: Arguments such as jobname for logger-filename
         """
-        method = Preprocess_ERA5_to_IFS.__call__.__name__
+        method = Preprocess_Unet_Tier1.__call__.__name__
 
         years = to_list(years)
         # sanity checks on years and season arguments
@@ -64,19 +67,25 @@ class Preprocess_ERA5_to_IFS(Abstract_Preprocessing):
             "%{0}: All elements of years must be numbers".format(method)
 
         years = [int(year) for year in years]
-        months = Preprocess_ERA5_to_IFS.check_season(season)
+        months = PreprocessERA5toIFS.check_season(season)
 
-        # instantiate PyStager
+        # initialize and set-up Pystager
         preprocess_pystager = PyStager(self.preprocess_worker, "year_month_list", nmax_warn=3)
+        preprocess_pystager.setup(years, months)
+
+        # Create grid description files needed for preprocessing (requires rank-information)
         self.my_rank = preprocess_pystager.my_rank
 
-        tar_grid_des_dict = Preprocess_ERA5_to_IFS.read_grid_des(self.grid_des_tar)
-        # use process_tar_grid_des-method from Preprocess_Unet_Tier1
-        base_gdes_d, coa_gdes_d = Preprocess_Unet_Tier1.process_tar_grid_des(self, tar_grid_des_dict, **kwargs)
-        gdes_dict = {"tar_grid_des": tar_grid_des_dict, "base_grid_des": base_gdes_d, "coa_grid_des": coa_gdes_d}
+        ifs_grid_des = CDOGridDes(self.grid_des_tar)
+        coa_gdes_d = ifs_grid_des.create_coarsened_grid_des(self.target_dir, self.downscaling_fac, self.my_rank,
+                                                            name_base="era5_", lextrapolate=False)
 
-        preprocess_pystager.setup(years, months)
-        preprocess_pystager.run(self.source_dir, self.target_dir, gdes_dict, job_name=jobname)
+        gdes_dict = {"tar_grid_des": ifs_grid_des.grid_des_dict, "coa_grid_des": coa_gdes_d}
+        # define arguments and keyword arguments for running PyStager later
+        run_dict = {"args": [self.source_dir_in, self.source_dir_out, self.target_dir, gdes_dict],
+                    "kwargs": {"job_name": kwargs.get("jobname", "Preproce_ERA5_to_IFS")}}
+
+        return preprocess_pystager, run_dict
 
     @staticmethod
     def preprocess_worker(year_months: List, dirin_era5: str, dirin_ifs: str, dirout: str, gdes_dict: dict,
@@ -92,7 +101,7 @@ class Preprocess_ERA5_to_IFS(Abstract_Preprocessing):
         :param nmax_warn: allowed maximum number of warnings/problems met during processing (default:3)
         :return: -
         """
-        method = Preprocess_ERA5_to_IFS.preprocess_worker.__name__
+        method = PreprocessERA5toIFS.preprocess_worker.__name__
 
         # sanity checks
         assert isinstance(logger, logging.Logger), "%{0}: logger-argument must be a logging.Logger instance" \
@@ -132,7 +141,7 @@ class Preprocess_ERA5_to_IFS(Abstract_Preprocessing):
         :param season: the seson string identifier
         :return: corresponding months as list of integers
         """
-        method = Preprocess_ERA5_to_IFS.check_season.__name__
+        method = PreprocessERA5toIFS.check_season.__name__
 
         known_seasons = ["DJF", "MMA", "JJA", "SON", "summer", "winter", "all"]
 
