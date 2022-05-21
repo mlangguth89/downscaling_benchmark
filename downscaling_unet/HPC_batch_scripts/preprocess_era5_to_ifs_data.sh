641f02b156ff126b24afbbe9612e7226245ad7ca
Michael <m.langguth@fz-juelich.de>
2022-05-21 14:24:01 +0200

    Update of preprocess_era5_to_ifs_data.sh .

--- downscaling_unet/HPC_batch_scripts/preprocess_era5_to_ifs_data.sh
+++ downscaling_unet/HPC_batch_scripts/preprocess_era5_to_ifs_data.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -x
 #SBATCH --account=deepacf
 #SBATCH --nodes=1
-#SBATCH --ntasks=7
+#SBATCH --ntasks=37
 ##SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --output=preprocess_era5_to_ifs_data-out.%j
 #SBATCH --error=preprocess_era5_to_ifs_data-err.%j
-#SBATCH --time=01:00:00
+#SBATCH --time=20:00:00
 #SBATCH --gres=gpu:0
-#SBATCH --partition=devel
+#SBATCH --partition=batch
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=m.langguth@fz-juelich.de
 ##jutil env activate -p cjjsc42
 
 # Name of virtual environment
-VIRT_ENV_NAME="venv_juwels"
+VIRT_ENV_NAME="venv_hdfml"
 
 # Activate virtual environment if needed (and possible)
 if [ -z ${VIRTUAL_ENV} ]; then
@@ -36,11 +36,11 @@ src_dir_ifs=/p/scratch/deepacf/maelstrom/maelstrom_data/ifs_hres/orig/
 out_dir=/p/scratch/deepacf/maelstrom/maelstrom_data/ap5_michael/preprocessed_era5_ifs/
 invar_file=/p/fastdata/slmet/slmet111/met_data/ecmwf/era5/grib/invariants/2020010100_invariants.grb
 # predictors, predictands and grid description file for target domain
-predictors='{"fc_sf": {"2t": "", "10u": "", "10v": "", "blh": ""}, "fc_pl": {"t": ["p85000","p92500"]}}'
+predictors='{"fc_sf": {"2t": "", "10u": "", "10v": "", "blh": "", "z": "", "sshf": "", "slhf": ""}, "fc_pl": {"t": ["p85000","p92500"]}}'
 predictands='{"sf": {"t2m": "", "z": ""}}'
 grid_des_tar=../grid_des/ifs_hres_grid_tar
 
-years=( 2017 )
+years=( 2016 2017 2018 )
 months=( 4 5 6 7 8 9 )
 method=ERA5_to_IFS
 
