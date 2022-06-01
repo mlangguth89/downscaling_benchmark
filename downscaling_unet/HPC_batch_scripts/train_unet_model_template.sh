a3287999a9334971c52365a239196dd8d24b3008
Michael <m.langguth@fz-juelich.de>
2022-06-01 15:57:50 +0200

    Add tempate identifier to train_unet_model_template.sh.

--- downscaling_unet/HPC_batch_scripts/train_unet_model_template.sh
+++ downscaling_unet/HPC_batch_scripts/train_unet_model_template.sh
@@ -12,6 +12,11 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=m.langguth@fz-juelich.de
 
+######### Template identifier (don't remove) #########
+echo "Do not run the template scripts"
+exit 99
+######### Template identifier (don't remove) #########
+
 # Name of virtual environment
 VIRT_ENV_NAME="venv_juwels"
 
