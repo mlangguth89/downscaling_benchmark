5f9efd4b31c4d677f1ad700afd83fb52672c4c56
Michael <m.langguth@fz-juelich.de>
2022-06-19 11:28:37 +0200

    Correct timestamps for querying RADKLIM data and include renaming of variables.

--- downscaling_ap5/preprocess/preprocess_radklim_ifs.py
+++ downscaling_ap5/preprocess/preprocess_radklim_ifs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
+# import modules
 import os, sys 
 import glob
 sys.path.append("../utils")
@@ -9,8 +9,33 @@ import xarray as xr
 import pandas as pd
 import datetime as dt
 from collections import OrderedDict
-from tools_utils import CDO, NCKS
+from typing import List
+from tools_utils import CDO, NCRENAME
 from other_utils import last_day_of_month
+
+
+# auxiliary function
+def add_varname_suffix(nc_file: str, varnames: List, suffix: str):
+    """
+    Rename variables in netCDF-file by adding a suffix
+    :param nc_file: netCDF-file to process
+    :param varnames: (old) variable names to modify
+    :param suffix: suffix to add to variable names
+    :return: status-flag
+    """
+    varnames_new = [varname + suffix for varname in varnames]
+    varnames_pair = ["{0},{1}".format(varnames[i], varnames_new[i]) for i in range(len(varnames))]
+
+    try:
+        ncrename.run([nc_file], OrderedDict([("-v", varnames_pair)]))
+        stat = True
+    except RuntimeError as err:
+        print("Could not rename all parsed variables: {0}".format(",".join(varnames)))
+        raise err
+
+    return stat
+
+# main
 
 # data-directories and parameters
 ifs_dir = "/p/scratch/deepacf/maelstrom/maelstrom_data/ifs_hres/orig"
@@ -31,7 +56,7 @@ ll_box_str = "{0:.3f},{1:.3f},{2:.3f},{3:.3f}".format(*lonlat_box)
 rd_var_lower = radklim_var.lower()
 
 # initialize tools
-cdo, ncks = CDO(), NCKS()
+cdo, ncrename = CDO(), NCRENAME()
 
 for yr in years:
     for mm in range(1, 13):
@@ -49,7 +74,7 @@ for yr in years:
         ld = last_day_of_month(dt.datetime.strptime("{0:d}{1:02d}01".format(yr, mm), "%Y%m%d"))
         init_runs = pd.date_range("{0:d}-{1:02d}-01 00:00".format(yr, mm),
                                   ld.replace(hour=12), freq="12H")
-        radklim_dates = pd.date_range(init_runs[0] + dt.timedelta(hours=6), init_runs[-1] + dt.timedelta(hours=6),
+        radklim_dates = pd.date_range(init_runs[0] + dt.timedelta(hours=6), init_runs[-1] + dt.timedelta(hours=17),
                                       freq="1H")
         # loop over all IFS forecast-files
         print("Start processing IFS-data for {0:d}-{1:02d}...".format(yr, mm))
@@ -60,10 +85,11 @@ for yr in years:
             
             ifs_pl_file, pl_file_out = ifs_sf_file.replace("sfc", "pl"), sf_file_out.replace("sfc", "pl")
 
-            cdo.run([ifs_sf_file, sf_file_out], OrderedDict([("-selname", ",".join(sf_vars)), ("-sellonlatbox", ll_box_str), 
-                                                             ("-seltimestep", "6/17")]))
-            cdo.run([ifs_pl_file, pl_file_out], OrderedDict([("-selname", ",".join(pl_vars)), ("-sellonlatbox", ll_box_str), 
-                                                             ("-seltimestep", "6/17"), ("-sellevel", "700")]))
+            cdo.run([ifs_sf_file, sf_file_out], OrderedDict([("-selname", ",".join(sf_vars)),
+                                                             ("-sellonlatbox", ll_box_str), ("-seltimestep", "6/17")]))
+            cdo.run([ifs_pl_file, pl_file_out], OrderedDict([("--reduce_dim", ""), ("-selname", ",".join(pl_vars)),
+                                                             ("-sellonlatbox", ll_box_str), ("-seltimestep", "6/17"),
+                                                             ("-sellevel", "700")]))
         
         # merge IFS forecast-files 
         all_sf_files, all_pl_files = glob.glob(os.path.join(outdir_tmp, "sfc_*.nc")), glob.glob(os.path.join(outdir_tmp, "pl_*.nc"))
@@ -72,6 +98,11 @@ for yr in years:
 
         cdo.run(all_sf_files + [sf_file], OrderedDict([("mergetime", "")]))
         cdo.run(all_pl_files + [pl_file], OrderedDict([("mergetime", "")]))
+
+        ncrename.run([pl_file], OrderedDict([("-v", ["u,u700", "v,v700"])]))
+        # rename variables
+        add_varname_suffix(sf_file, sf_vars, "_in")
+        add_varname_suffix(pl_file, pl_vars, "_in")
         # loop over all corresponding RADKLIM timesteps
         print("Start processing RADKLIM-data for {0:d}-{1:02d}...".format(yr, mm))
         for rd in radklim_dates:
@@ -104,5 +135,9 @@ for yr in years:
         
         cdo.run(all_rd_lres_files + [rd_lres_file], OrderedDict([("mergetime", "")]))
         cdo.run(all_rd_hres_files + [rd_hres_file], OrderedDict([("mergetime", "")]))
+
+        # rename variables
+        add_varname_suffix(rd_lres_file, [radklim_var], "_in")
+        add_varname_suffix(rd_hres_file, [radklim_var], "_tar")
                                           
         
