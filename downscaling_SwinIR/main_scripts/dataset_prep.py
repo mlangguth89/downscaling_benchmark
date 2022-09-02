3cf7b2768513bd549182ff7a7d19367b01e232cb
maxim <maximbr@post.bgu.ac.il>
2022-09-02 11:10:00 +0200

    Add a new data generator for temperature

--- downscaling_SwinIR/main_scripts/dataset_prep.py
+++ downscaling_SwinIR/main_scripts/dataset_prep.py
@@ -7,7 +7,6 @@ __email__ = "b.gong@fz-juelich.de"
 __author__ = "Bing Gong"
 __date__ = "2022-07-13"
 
-
 import xarray as xr
 import torch
 import numpy as np
@@ -16,6 +15,7 @@ import math
 import torchvision
 import os
 import json
+
 
 class PrecipDatasetInter(torch.utils.data.IterableDataset):
     """
@@ -47,12 +47,12 @@ class PrecipDatasetInter(torch.utils.data.IterableDataset):
         self.var_out = var_out
         self.batch_size = batch_size
         self.seed = seed
-        self.k = k 
+        self.k = k
         self.vars_in_patches_list = []
         self.vars_out_patches_list = []
         self.times_patches_list = []
-        
-        prcpids = ['yw','cp','lsp']
+
+        prcpids = ['yw', 'cp', 'lsp']
         self.prcp_indexes = []
         i = 0
 
@@ -64,26 +64,26 @@ class PrecipDatasetInter(torch.utils.data.IterableDataset):
 
         # Search for files
         p = pathlib.Path(self.file_path)
-        assert(p.is_dir())
-        #self.files = glob.glob(os.path.join(file_path, 'preproc_ifs_radklim*.nc'))
-        #for path in p.rglob('preproc_ifs_radklim*.nc'):
+        assert (p.is_dir())
+        # self.files = glob.glob(os.path.join(file_path, 'preproc_ifs_radklim*.nc'))
+        # for path in p.rglob('preproc_ifs_radklim*.nc'):
         #    print("pathname",path.name)
         files = sorted(p.rglob('preproc_ifs_radklim_*.nc'))
         if len(files) < 1:
             raise RuntimeError('No files found.')
         print("Going to open the following files:", files)
 
-        self.vars_in_patches_list, self.vars_out_patches_list, self.times_patches_list  = self.process_netcdf(files)
+        self.vars_in_patches_list, self.vars_out_patches_list, self.times_patches_list = self.process_netcdf(files)
 
-        self.vars_in_patches_mean = self.vars_in_patches_list.mean(dim=(0,2,3))
-        self.vars_in_patches_std = self.vars_in_patches_list.std(dim=(0,2,3))
+        self.vars_in_patches_mean = self.vars_in_patches_list.mean(dim=(0, 2, 3))
+        self.vars_in_patches_std = self.vars_in_patches_list.std(dim=(0, 2, 3))
         self.vars_out_patches_mean = self.vars_out_patches_list.mean()
         self.vars_out_patches_std = self.vars_out_patches_list.std()
 
         print("The total number of samples after filtering NaN values:", len(self.vars_in_patches_list))
-        
+
         self.n_samples = len(self.vars_in_patches_list)
-        #print("var_out size",self.vars_out_patches_list)
+        # print("var_out size",self.vars_out_patches_list)
 
         self.idx_perm = self.shuffle()
         self.save_stats()
@@ -95,8 +95,8 @@ class PrecipDatasetInter(torch.utils.data.IterableDataset):
         print("Loading data from the file:", filenames)
         dt = xr.open_mfdataset(filenames)
         # get input variables, and select the regions
-        inputs = dt[self.vars_in].isel(lon = slice(2, 114)).sel(lat = slice(47.5, 60))
-        output = dt[self.var_out].isel(lon_tar = slice(16, 113 * 10 + 6)).sel(lat_tar = slice(47.41, 60))
+        inputs = dt[self.vars_in].isel(lon=slice(2, 114)).sel(lat=slice(47.5, 60))
+        output = dt[self.var_out].isel(lon_tar=slice(16, 113 * 10 + 6)).sel(lat_tar=slice(47.41, 60))
 
         n_lat = inputs["lat"].values.shape[0]
         n_lon = inputs["lon"].values.shape[0]
@@ -108,20 +108,20 @@ class PrecipDatasetInter(torch.utils.data.IterableDataset):
         n_patches_y = int(np.floor(n_lat) / self.patch_size)
         num_patches_img = n_patches_x * n_patches_y
 
-        da_in = torch.from_numpy(inputs.to_array(dim = "variables").squeeze().values)
-        da_out = torch.from_numpy(output.to_array(dim = "variables").squeeze().values)
+        da_in = torch.from_numpy(inputs.to_array(dim="variables").squeeze().values)
+        da_out = torch.from_numpy(output.to_array(dim="variables").squeeze().values)
         times = inputs["time"].values  # get the timestamps
-        times = np.transpose(np.stack([dt["time"].dt.year,dt["time"].dt.month,dt["time"].dt.day,dt["time"].dt.hour]))        
-        
+        times = np.transpose(np.stack([dt["time"].dt.year, dt["time"].dt.month, dt["time"].dt.day, dt["time"].dt.hour]))
+
         print("Original input shape:", da_in.shape)
 
         # split into small patches, the return dim are [vars, samples,n_patch_x, n_patch_y, patch_size, patch_size]
         vars_in_patches = da_in.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
         vars_in_patches_shape = list(vars_in_patches.shape)
 
-        #sanity check to make sure the number of patches is as we expected
+        # sanity check to make sure the number of patches is as we expected
         assert n_patches_x * n_patches_y == int(vars_in_patches_shape[2] * vars_in_patches_shape[3])
-        
+
         vars_in_patches = torch.reshape(vars_in_patches, [vars_in_patches_shape[0],
                                                           vars_in_patches_shape[1] * vars_in_patches_shape[2] *
                                                           vars_in_patches_shape[3],
@@ -131,15 +131,15 @@ class PrecipDatasetInter(torch.utils.data.IterableDataset):
         print("Input shape:", vars_in_patches.shape)
 
         ## Replicate times for patches in the same image
-        times_patches = torch.from_numpy(np.array([ x for x in times for _ in range(num_patches_img)]))
-        
-        ## sanity check 
-        assert len(times_patches) ==  vars_in_patches_shape[1] * vars_in_patches_shape[2] * vars_in_patches_shape[3]
+        times_patches = torch.from_numpy(np.array([x for x in times for _ in range(num_patches_img)]))
+
+        ## sanity check
+        assert len(times_patches) == vars_in_patches_shape[1] * vars_in_patches_shape[2] * vars_in_patches_shape[3]
 
         vars_out_patches = da_out.unfold(1, self.patch_size * self.sf,
                                          self.patch_size * self.sf).unfold(2,
-                                                                       self.patch_size * self.sf,
-                                                                       self.patch_size * self.sf)
+                                                                           self.patch_size * self.sf,
+                                                                           self.patch_size * self.sf)
         vars_out_patches_shape = list(vars_out_patches.shape)
         vars_out_patches = torch.reshape(vars_out_patches,
                                          [vars_out_patches_shape[0] * vars_out_patches_shape[1] *
@@ -161,68 +161,69 @@ class PrecipDatasetInter(torch.utils.data.IterableDataset):
         # Only get the patch that without NaN values
         vars_out_pathes_no_nan = torch.index_select(vars_out_patches, 0, no_nan_idx)
         vars_in_patches_no_nan = torch.index_select(vars_in_patches, 0, no_nan_idx)
-        times_no_nan = torch.index_select(times_patches,0, no_nan_idx) 
+        times_no_nan = torch.index_select(times_patches, 0, no_nan_idx)
         assert len(vars_out_pathes_no_nan) == len(vars_in_patches_no_nan)
 
         # log-transform -> log(x+k)-log(k)
-        vars_in_patches_no_nan[:,self.prcp_indexes,:,:] = torch.log(vars_in_patches_no_nan[:,self.prcp_indexes,:,:]+torch.tensor(self.k))-torch.log(torch.tensor(self.k))
-        vars_out_pathes_no_nan = torch.log(vars_out_pathes_no_nan+torch.tensor(self.k))-torch.log(torch.tensor(self.k))
+        vars_in_patches_no_nan[:, self.prcp_indexes, :, :] = torch.log(
+            vars_in_patches_no_nan[:, self.prcp_indexes, :, :] + torch.tensor(self.k)) - torch.log(torch.tensor(self.k))
+        vars_out_pathes_no_nan = torch.log(vars_out_pathes_no_nan + torch.tensor(self.k)) - torch.log(
+            torch.tensor(self.k))
 
         return vars_in_patches_no_nan, vars_out_pathes_no_nan, times_no_nan
 
     def shuffle(self):
         """
-        shuffle the index 
+        shuffle the index
         """
         print("Shuffling the index ....")
         multiformer_np_rng = np.random.default_rng(self.seed)
         idx_perm = multiformer_np_rng.permutation(self.n_samples)
 
         # restrict to multiples of batch size
-        idx = int(math.floor(self.n_samples/self.batch_size)) * self.batch_size
+        idx = int(math.floor(self.n_samples / self.batch_size)) * self.batch_size
 
         idx_perm = idx_perm[:idx]
-        print("idx_perm",idx_perm)
+        print("idx_perm", idx_perm)
         return idx_perm
 
     def save_stats(self):
         output_file = os.path.join(self.file_path, "statistics.json")
         stats = {}
         for i in range(len(self.vars_in)):
-            key = self.vars_in[i]+'_mean'
-            stats.update({key:float(self.vars_in_patches_mean[i])})
-            key = self.vars_in[i]+'_std'
-            stats.update({key:float(self.vars_in_patches_std[i])}) 
-            
-        key = self.var_out[0]+'_mean'
-        stats.update({key:float(self.vars_out_patches_mean)})
-        key = self.var_out[0]+'_std'
-        stats.update({key:float(self.vars_out_patches_std)})
+            key = self.vars_in[i] + '_mean'
+            stats.update({key: float(self.vars_in_patches_mean[i])})
+            key = self.vars_in[i] + '_std'
+            stats.update({key: float(self.vars_in_patches_std[i])})
 
-        #save to output directory
-        with open(output_file,'w') as f:
+        key = self.var_out[0] + '_mean'
+        stats.update({key: float(self.vars_out_patches_mean)})
+        key = self.var_out[0] + '_std'
+        stats.update({key: float(self.vars_out_patches_std)})
+
+        # save to output directory
+        with open(output_file, 'w') as f:
             json.dump(stats, f)
         print("The statistic has been stored to the json file: ", output_file)
 
     def __iter__(self):
 
-        iter_start, iter_end = 0, int(len(self.idx_perm)/self.batch_size)  # todo
+        iter_start, iter_end = 0, int(len(self.idx_perm) / self.batch_size)
         self.idx = 0
 
-        transform_x = torchvision.transforms.Normalize(self.vars_in_patches_mean,self.vars_in_patches_std)
+        transform_x = torchvision.transforms.Normalize(self.vars_in_patches_mean, self.vars_in_patches_std)
 
         for bidx in range(iter_start, iter_end):
 
-            #initialise x, y for each batch
+            # initialise x, y for each batch
             # x  stores the low resolution images, y for high resolution,
             # t is the corresponding timestamps, cidx is the index
             x = torch.zeros(self.batch_size, len(self.vars_in), self.patch_size, self.patch_size)
-            y = torch.zeros(self.batch_size, self.patch_size * self.sf, self.patch_size * self.sf )
-            t = torch.zeros(self.batch_size, 4, dtype = torch.int)
-            cidx = torch.zeros(self.batch_size, 1, dtype = torch.int) #store the index
+            y = torch.zeros(self.batch_size, self.patch_size * self.sf, self.patch_size * self.sf)
+            t = torch.zeros(self.batch_size, 4, dtype=torch.int)
+            cidx = torch.zeros(self.batch_size, 1, dtype=torch.int)  # store the index
 
             for jj in range(self.batch_size):
-
                 cid = self.idx_perm[self.idx]
 
                 x[jj] = transform_x(self.vars_in_patches_list[cid])
@@ -231,7 +232,8 @@ class PrecipDatasetInter(torch.utils.data.IterableDataset):
                 cidx[jj] = torch.tensor(cid, dtype=torch.int)
 
                 self.idx += 1
-            yield  {'L': x, 'H': y, "idx": cidx, "T":t}
+            yield {'L': x, 'H': y, "idx": cidx, "T": t}
+
 
 def run():
     data_loader = PrecipDatasetInter(file_path="/p/scratch/deepacf/deeprain/bing/downscaling_maelstrom/train")
@@ -247,15 +249,6 @@ def run():
         print("batch_idx", batch_idx)
         print("timestamps,", times)
 
+
 if __name__ == "__main__":
     run()
-
-
-
-
-
-
-
-
-
-
---
+++ downscaling_SwinIR/main_scripts/dataset_temp.py
@@ -0,0 +1,198 @@
+# SPDX-FileCopyrightText: 2021 Earth System Data Exploration (ESDE), Jülich Supercomputing Center (JSC)
+#
+# SPDX-License-Identifier: MIT
+
+
+__email__ = "b.gong@fz-juelich.de"
+__author__ = "Maxim Bragilovski"
+__date__ = "2022-07-13"
+
+import xarray as xr
+import torch
+import numpy as np
+import pathlib
+import math
+import torchvision
+import os
+import json
+from downscaling_SwinIR.handle_data.handle_data_unet import HandleUnetData
+
+
+class TempDatasetInter(torch.utils.data.IterableDataset):
+    """
+    This is the class used for generate dataset generator for precipitation downscaling
+    """
+
+    def __init__(self, file_path: str = None, batch_size: int = 4, verbose: int = 0, seed: int = 1234):
+        """
+        file_path : the path to the .nc dataset
+        batch_size: the number of samples per iteration
+        verbose: specify the kind of the dataset, 0 for train and 1 for validation
+        seed      : specify a seed so that we can generate the same random index for shuffle function
+        """
+
+        super(TempDatasetInter).__init__()
+
+        self.ds_tar = None
+        self.ds_in = None
+        self.file_path = file_path
+        self.batch_size = batch_size
+        self.verbose = verbose
+        self.seed = seed
+        self.ds = xr.open_dataset(file_path)
+
+        self.ds.load()
+        self.times = np.transpose(np.stack(
+            [self.ds["time"].dt.year, self.ds["time"].dt.month, self.ds["time"].dt.day, self.ds["time"].dt.hour]))
+        self.process_era5_netcdf()
+
+        print("The total number of samples are:", self.ds.sizes['time'])
+
+        self.n_samples = self.ds.sizes['time']
+        self.idx_perm = self.shuffle()
+
+        self.log = self.ds.sizes['rlon']
+        self.lat = self.ds.sizes['rlat']
+        # self.save_stats()
+
+    def process_era5_netcdf(self):
+        """
+        process netcdf files: normalization,
+        """
+
+        def reshape_ds(ds):
+            da = ds.to_array(dim="variables")
+            da = da.transpose(..., "variables")
+            return da
+
+        da_train = reshape_ds(self.ds)
+        norm_dims = ["time", "rlat", "rlon"]
+
+        if self.verbose == 0:
+            da_norm, mu, std = HandleUnetData.z_norm_data(da_train, dims=norm_dims, return_stat=True)
+            for save in [(mu, 'mu'), (std, 'std')]:
+                self.save_stats(save[0], save[1])
+        if self.verbose == 1:
+            mu_train = self.load_stats('mu')
+            std_train = self.load_stats('std')
+            da_norm = HandleUnetData.z_norm_data(da_train, mu=mu_train, std=std_train)
+
+        da_norm = da_norm.astype(np.float32)
+
+        def gen(darr_in, darr_tar):
+            ds_train_in = []
+            ds_train_tar = []
+            ntimes = len(darr_in["time"])
+            for t in range(ntimes):
+                ds_train_in.append(darr_in.isel({"time": t}).values)
+                ds_train_tar.append(darr_tar.isel({"time": t}).values)
+            return ds_train_in, ds_train_tar
+
+        da_in, da_tar = split_in_tar(da_norm)
+        self.ds_in, self.ds_tar = gen(da_in, da_tar)
+
+    def shuffle(self):
+        """
+        shuffle the index
+        """
+        print("Shuffling the index ....")
+        multiformer_np_rng = np.random.default_rng(self.seed)
+        idx_perm = multiformer_np_rng.permutation(self.n_samples)
+
+        # restrict to multiples of batch size
+        idx = int(math.floor(self.n_samples / self.batch_size)) * self.batch_size
+
+        idx_perm = idx_perm[:idx]
+
+        return idx_perm
+
+    def save_stats(self, to_save, name):
+        dict_to_save = to_save.to_dict()
+        # json_object = json.dump(dict_to_save)
+        path = self.file_path.split('\\')
+        path = '\\'.join(e for e in path[0:len(path) - 1]) + '\\' + name + '.json'
+        with open(path, 'w') as f:
+            json.dump(dict_to_save, f)
+
+    def load_stats(self, name):
+        path = self.file_path.split('\\')
+        path = '\\'.join(e for e in path[0:len(path) - 1]) + '\\' + name + '.json'
+        with open(path) as json_file:
+            data = json.load(json_file)
+
+        for key in data.keys():
+            print(key)
+        ds = xr.DataArray.from_dict(data)
+        return xr.DataArray.from_dict(data)
+
+    def __iter__(self):
+
+        iter_start, iter_end = 0, int(len(self.idx_perm) / self.batch_size)
+        self.idx = 0
+
+        for bidx in range(iter_start, iter_end):
+
+            # initialise x, y for each batch
+            # x  stores the low resolution images, y for high resolution,
+            # t is the corresponding timestamps, cidx is the index
+            x = torch.zeros(self.batch_size, 9, self.lat, self.log)
+            y = torch.zeros(self.batch_size, 2, self.lat, self.log)
+            t = torch.zeros(self.batch_size, 4, dtype=torch.int)
+            cidx = torch.zeros(self.batch_size, 1, dtype=torch.int)  # store the index
+
+            for jj in range(self.batch_size):
+                cid = self.idx_perm[self.idx]
+
+                x[jj] = torch.from_numpy(self.ds_in[cid]).permute(2, 0, 1)
+                y[jj] = torch.from_numpy(self.ds_tar[cid]).permute(2, 0, 1)
+                t[jj] = torch.from_numpy(self.times[cid])
+                cidx[jj] = torch.tensor(cid, dtype=torch.int)
+
+                self.idx += 1
+            yield {'L': x, 'H': y, "idx": cidx, "T": t}
+
+
+def run():
+    data_loader = TempDatasetInter(
+        file_path="C:\\Users\\max_b\\PycharmProjects\\downscaling_maelstrom\\preproc_era5_crea6_small.nc")
+    print("created data_loader")
+    for batch_idx, train_data in enumerate(data_loader):
+        inputs = train_data["L"]
+        target = train_data["H"]
+        idx = train_data["idx"]
+        times = train_data["T"]
+        print("inputs", inputs.size())
+        print("target", target.size())
+        print("idx", idx)
+        print("batch_idx", batch_idx)
+        print("timestamps,", times)
+
+
+@staticmethod
+def split_in_tar(da: xr.DataArray, target_var: str = "t2m") -> (xr.DataArray, xr.DataArray):
+    """
+    Split data array with variables-dimension into input and target data for downscaling.
+    :param da: The unsplitted data array.
+    :param target_var: Name of target variable which should consttute the first channel
+    :return: The splitted data array.
+    """
+    invars = [var for var in da["variables"].values if var.endswith("_in")]
+    tarvars = [var for var in da["variables"].values if var.endswith("_tar")]
+
+    # ensure that ds_tar has a channel coordinate even in case of single target variable
+    roll = False
+    if len(tarvars) == 1:
+        sl_tarvars = tarvars
+    else:
+        sl_tarvars = slice(*tarvars)
+        if tarvars[0] != target_var:  # ensure that target variable appears as first channel
+            roll = True
+
+    da_in, da_tar = da.sel({"variables": invars}), da.sel(variables=sl_tarvars)
+    if roll: da_tar = da_tar.roll(variables=1, roll_coords=True)
+
+    return da_in, da_tar
+
+
+if __name__ == "__main__":
+    run()
