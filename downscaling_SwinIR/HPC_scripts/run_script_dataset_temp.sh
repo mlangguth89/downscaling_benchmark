d041d52eb986e597a96108956348383a0f511fd4
maxim <maximbr@post.bgu.ac.il>
2022-09-05 16:51:16 +0200

    fix bug

--- downscaling_SwinIR/HPC_scripts/run_script_dataset_temp.sh
+++ downscaling_SwinIR/HPC_scripts/run_script_dataset_temp.sh
@@ -6,7 +6,7 @@
 #SBATCH --error=train-err.%j
 #SBATCH --time=02:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --partition=develgpus
+#SBATCH --partition=develbooster
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=maximbr@post.bgu.ac.il
 
