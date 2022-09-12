9961884c668236a091240825e08c438b0e352cc5
maxim <maximbr@post.bgu.ac.il>
2022-09-12 11:25:13 +0200

    adapted running on gpu

--- downscaling_SwinIR/main_scripts/main_train_temprture.py
+++ downscaling_SwinIR/main_scripts/main_train_temprture.py
@@ -27,7 +27,7 @@ CHECKPOINT_SAVE = 200 # how many steps to save checkpoint
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Get data
-fl = "/p/scratch/deepacf/maelstrom/maelstrom_data/ap5_michael/preprocessed_era5_crea6/netcdf_data/all_files/preproc_era5_crea6_val.nc" #C:\\Users\\max_b\\PycharmProjects\\downscaling_maelstrom\\preproc_era5_crea6_small.nc
+fl = "/p/scratch/deepacf/maelstrom/maelstrom_data/ap5_michael/preprocessed_era5_crea6/netcdf_data/all_files/preproc_era5_crea6_train.nc" #C:\\Users\\max_b\\PycharmProjects\\downscaling_maelstrom\\preproc_era5_crea6_small.nc
 # train_dt = xr.open_dataset(fl)
 # fl_test = "/p/scratch/deepacf/maelstrom/maelstrom_data/ap5_michael/preprocessed_era5_crea6/netcdf_data/all_files/preproc_era5_crea6_val.nc"
 # test_dt = xr.open_dataset(fl_test)
