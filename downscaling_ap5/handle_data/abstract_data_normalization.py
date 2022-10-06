cfd2fb1030cd5631695911ecdff5945e49d0b6cf
Michael <m.langguth@fz-juelich.de>
2022-10-06 17:13:45 +0200

    Add new scripts for normalization class to repo.

---
+++ downscaling_ap5/handle_data/abstract_data_normalization.py
@@ -0,0 +1,147 @@
+# SPDX-FileCopyrightText: 2022 Earth System Data Exploration (ESDE), Jülich Supercomputing Center (JSC)
+#
+# SPDX-License-Identifier: MIT
+
+"""
+Abstract class to perform normalization on data
+"""
+
+__email__ = "m.langguth@fz-juelich.de"
+__author__ = "Michael Langguth"
+__date__ = "2022-10-06"
+
+from abc import ABC, abstractmethod
+from typing import Union, List
+import json as js
+import numpy as np
+import xarray as xr
+
+da_or_ds = Union[xr.DataArray, xr.Dataset]
+
+
+class Normalize(ABC):
+    """
+    Abstract class for normalizing data.
+    """
+    def __init__(self, method: str, norm_dims: List):
+        self.method = method
+        self.norm_dims = norm_dims
+        self.norm_stats = None
+
+    def normalize(self, data: xr.DataArray, **stats):
+        """
+        Normalize data.
+        :param data: The DataArray to be normalized.
+        :param **stats: Parameters to perform normalization. Must fit to normalization type!
+        :return: DataArray with normalized data.
+        """
+        # sanity checks
+        if not isinstance(data, xr.DataArray):
+            raise TypeError(f"Passed data must be a xarray.DataArray, but is of type {str(type(data))}.")
+
+        _ = self._check_norm_dims(data)
+        # do the computation
+        norm_stats = self.get_required_stats(data, **stats)
+        data_norm = self.normalize_data(data, *norm_stats)
+
+        return data_norm
+
+    def denormalize(self, data: da_or_ds, **stats):
+        """
+        Denormalize data.
+        :param data: The DataArray to be denormalized.
+        :param **stats: Parameters to perform denormalization. Must fit to normalization type!
+        :return: DataArray with denormalized data.
+        """
+        # sanity checks
+        if not isinstance(data, xr.DataArray):
+            raise TypeError(f"Passed data must be a xarray.DataArray, but is of type {str(type(data))}.")
+
+        _ = self._check_norm_dims(data)
+        # do the computation
+        norm_stats = self.get_required_stats(data, *stats)
+        data_denorm = self.denormalize_data(data, *norm_stats)
+
+        return data_denorm
+
+    @property
+    def norm_dims(self):
+        return self._norm_dims
+
+    @norm_dims.setter
+    def norm_dims(self, norm_dims):
+        if norm_dims is None:
+            raise AttributeError("norm_dims must not be None. Please parse a list of dimensions" +
+                                 "over which normalization should be applied.")
+
+        self._norm_dims = list(norm_dims)
+
+    def _check_norm_dims(self, data):
+        """
+        Check if dimension for normalization reside in dimensions of data.
+        :param data: the data (xr.DataArray) to be normalized
+        :return True: in case of passed check, a ValueError is risen else
+        """
+        data_dims = list(data.dims)
+        norm_dims_check = [norm_dim in data_dims for norm_dim in self.norm_dims]
+        if not all(norm_dims_check):
+            imiss = np.where(~np.array(norm_dims_check))[0]
+            miss_dims = list(np.array(self.norm_dims)[imiss])
+            raise ValueError("The following dimensions do not reside in the data: " +
+                             f"{', '.join(miss_dims)}")
+
+        return True
+
+    def save_norm_to_file(self, js_file):
+        """
+        Write normalization parameters to file.
+        :param js_file: Path to JSON-file to be created.
+        :return: -
+        """
+        if self.norm_stats is None:
+            raise AttributeError("norm_stats is still None. Please run (de-)normalization to get parameters.")
+
+        if any([stat is None for stat in self.norm_stats.values()]):
+            raise AttributeError("Some parameters of norm_stats are None.")
+
+        norm_serialized = {key: da.to_dict() for key, da in self.norm_stats.items()}
+
+        with open(js_file, "w") as jsf:
+            js.dump(norm_serialized, jsf)
+
+    def read_norm_from_file(self, js_file):
+        """
+        Read normalization parameters from file. Inverse function to write_norm_from_file.
+        :param js_file: Path to JSON-file to be read.
+        :return: Parameters set to self.norm_stats
+        """
+        with open(js_file, "r") as jsf:
+            norm_data = js.load(jsf)
+
+        norm_dict_restored = {key: xr.DataArray.from_dict(da_dict) for key, da_dict in norm_data.items()}
+
+        self.norm_stats = norm_dict_restored
+
+    @abstractmethod
+    def get_required_stats(self, data, *stats):
+        """
+        Function to retrieve either normalization parameters from data or from keyword arguments
+        """
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def normalize_data(data, *norm_param):
+        """
+        Function to normalize data.
+        """
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def denormalize_data(data, *norm_param):
+        """
+        Function to denormalize data.
+        """
+        pass
+
