bdec908e661d6eb3117eb28cf9ae2554e91f5733
Michael <m.langguth@fz-juelich.de>
2022-10-07 15:51:06 +0200

    First complete template of main_train.py.

--- downscaling_ap5/main_scripts/main_train.py
+++ downscaling_ap5/main_scripts/main_train.py
@@ -24,7 +24,7 @@ import xarray as xr
 import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.python.keras.utils.layer_utils import count_params
-from model_utils import get_model
+from model_utils import ModelEngine
 from handle_data_class import HandleDataClass, get_dataset_filename
 from all_normalizations import ZScore
 from benchmark_utils import BenchmarkCSV, get_training_time_dict
@@ -64,21 +64,20 @@ def main(parser_args):
     except Exception as err:
         raise err
 
-    # get model object or tuple of model objects for composite models
-    model_obj = get_model(parser_args.model)
+    # get model instance and path to data files
+    model_instance = ModelEngine(parser_args.model_name)
     fdata_train, fdata_val = get_dataset_filename(datadir, dataset, "training"), \
                              get_dataset_filename(datadir, dataset, "validation")
 
     # initialize benchmarking object
     bm_obj = BenchmarkCSV(os.path.join(os.getcwd(), f"benchmark_training_{parser_args.model}.csv"))
+
+    # Read data from disk and preprocess (normalization and conversion to TF dataset)
     print("Start reading data from disk...")
     t0_save = timer()
     ds_train, ds_val = xr.open_dataset(fdata_train), xr.open_dataset(fdata_val)
 
     benchmark_dict = {"loading data time": timer() - t0_save}
-
-    model_instance = GetModel(parser_args.model_name)
-    model = model_instance.instantiate(parser_args.exp_name, model_savedir, hparams_dict)
 
     # prepare training and validation data
     t0_preproc = timer()
@@ -95,6 +94,9 @@ def main(parser_args):
     tfds_val = HandleDataClass.make_tf_dataset(da_val, ds_dict["batch_size"], lshuffle=False,
                                                var_tar2in=ds_dict["var_tar2in"])
 
+    nsamples = da_train.shape[0]
+    shape_in = tfds_train.element_spec[0].shape[1:]
+
     # clean up to save some memory
     del ds_train
     del ds_val
@@ -102,6 +104,17 @@ def main(parser_args):
 
     t0_compile = timer()
     benchmark_dict["preprocessing data time"] = t0_compile - t0_preproc
+
+    # instantiate and compile model
+    model = model_instance(hparams_dict, parser_args.exp_name, model_savedir)
+
+    func_compile_opt = getattr(model, "get_compile_options", None)
+    if callable(func_compile_opt):
+        compile_opts = func_compile_opt()
+    else:
+        compile_opts = {}
+
+    model.compile(shape_in, **compile_opts)
 
     # train model
     # define class for creating timer callback
@@ -115,108 +128,35 @@ def main(parser_args):
         def on_epoch_end(self, epoch, logs={}):
             self.epoch_times.append(timer() - self.epoch_time_start)
 
+    time_tracker = TimeHistory()
+    callback_list = [time_tracker]
 
-
-
-
-
-
-
-
-
-
-
-    os.makedirs(model_savedir, exist_ok=True)
-    # Read training and validation data
-
-
-    # handle (hyper-) parameters
-    keys_remove = ["input_dir", "output_dir", "id", "no_z_branch", "no_z_tar2in", "model_name", "no_supervision"]
-    args_dict = {k: v for k, v in vars(parser_args).items() if (v is not None) & (k not in keys_remove)}
-    args_dict["z_branch"] = not parser_args.no_z_branch
-    if not parser_args.no_z_tar2in:  # add high-resolved surface topography to preditor variable list
-        args_dict["var_tar2in"] = "hsurf_tar"
-    supervise = not parser_args.no_supervision
-    # set critic learning rate equal to generator if not supplied
-    if not "lr_critic": args_dict["lr_critic"] = args_dict["lr_gen"]
-
-    # instantiate model
-    wgan_model = WGAN(build_unet, critic_model, args_dict, model_name, supervise, model_savedir)
-
-    # prepare training and validation data
-    t0_preproc = timer()
-
-    # slice data temporaly
-    # ds_train = ds_train.sel(time=slice("2014-01-01", "2016-12-30"))
-    da_train, da_val = HandleUnetData.reshape_ds(ds_train), HandleUnetData.reshape_ds(ds_val)
-
-    norm_dims = ["time", "rlat", "rlon"]
-    da_train, mu_train, std_train = HandleUnetData.z_norm_data(da_train, dims=norm_dims,
-                                                               save_path=os.path.join(outdir, parser_args.model_name),
-                                                               return_stat=True)
-    da_val = HandleUnetData.z_norm_data(da_val, mu=mu_train, std=std_train)
-
-    # clean up to save some memory
-    del ds_train
-    del ds_val
-    gc.collect()
-
-    t0_compile = timer()
-    benchmark_dict["preprocessing data time"] = t0_compile - t0_preproc
-
-    # compile model and get TF datasets for training and validation
-    print("Start compiling WGAN-model.")
-    train_iter, val_iter = wgan_model.compile(da_train.astype(np.float32), da_val.astype(np.float32))
-
-    benchmark_dict["model compile time"] = timer() - t0_compile
-
-    # train model
-    # define class for creating timer callback
-    class TimeHistory(keras.callbacks.Callback):
-        def on_train_begin(self, logs={}):
-            self.epoch_times = []
-
-        def on_epoch_begin(self, epoch, logs={}):
-            self.epoch_time_start = timer()
-
-        def on_epoch_end(self, epoch, logs={}):
-            self.epoch_times.append(timer() - self.epoch_time_start)
-
-    class LRLogger(keras.callbacks.Callback):
-        def __init__(self):
-            super().__init__()
-            self._supports_tf_logs = True
-
-        def on_train_begin(self, logs={}):
-            self.lr_gen_logs = []
-            self.lr_critic_logs = []
-
-        def on_epoch_begin(self, epoch, logs={}):
-            self.lr_gen_logs.append(self.model.g_optimizer._decayed_lr(tf.float32))
-            self.lr_critic_logs.append(self.model.c_optimizer._decayed_lr(tf.float32))
-
-    # create callbacks for scheduling learning rate and for timing training process
-    time_tracker, lr_tracker = TimeHistory(), LRLogger()
-    callback_list = [time_tracker, lr_tracker]
+    # retrieve customize fit-options if required
+    func_fit_opt = getattr(model, "get_fit_options", None)
+    if callable(func_fit_opt):
+        fit_opts = func_fit_opt()
+    else:
+        fit_opts = {}
 
     print("Start training of WGAN...")
-    history = wgan_model.fit(train_iter, val_iter, callbacks=callback_list)
+    history = model.fit(tfds_train, tfds_val, callbacks=callback_list, **fit_opts)
 
     # get some parameters from tracked training times and put to dictionary
     training_times = get_training_time_dict(time_tracker.epoch_times,
-                                            wgan_model.nsamples * wgan_model.hparams["train_epochs"])
+                                            model.nsamples * hparams_dict["train_epochs"])
     benchmark_dict = {**benchmark_dict, **training_times}
 
-    print("WGAN training finished. Save model to '{0}' and start creating example plot."
-          .format(os.path.join(outdir, parser_args.model_name)))
-    # save trained model (generator and critic are saved seperately)
+    print(f"Training of model '{parser_args.model_name}' training finished. Save model to '{model_savedir}'")
+
+    # save trained model
     t0_save = timer()
 
-    wgan_model.generator.save(os.path.join(model_savedir, "{0}_generator_last".format(parser_args.model_name)))
-    wgan_model.critic.save(os.path.join(model_savedir, "{0}_critic_last".format(parser_args.model_name)))
+    os.makedirs(model_savedir, exist_ok=True)
+    model.save(model_savedir)
 
     tend = timer()
     benchmark_dict["saving model time"] = tend - t0_save
+
     # finally, track total runtime...
     benchmark_dict["total runtime"] = tend - t0
     benchmark_dict["job id"] = job_id
@@ -231,15 +171,15 @@ def main(parser_args):
 
     js_file = os.path.join(model_savedir, "benchmark_training_static.json")
     if not os.path.isfile(js_file):
-        stat_info = {"static_model_info":
-                         {"trainable_parameters_generator": count_params(wgan_model.generator.trainable_weights),
-                          "non-trainable_parameters_generator": count_params(
-                              wgan_model.generator.non_trainable_weights),
-                          "trainable_parameters_critc": count_params(wgan_model.critic.trainable_weights),
-                          "non-trainable_parameters_critic": count_params(wgan_model.critic.non_trainable_weights)},
+        func_model_info = getattr(model, "get_model_info", None)
+        if callable(func_model_info):
+            model_info = func_model_info()
+        else:
+            model_info = {}
+        stat_info = {"static_model_info": model_info,
                      "data_info": {"training data size": da_train.nbytes, "validation data size": da_val.nbytes,
-                                   "nsamples": wgan_model.nsamples, "shape_samples": wgan_model.shape_in,
-                                   "batch_size": wgan_model.hparams["batch_size"]}}
+                                   "nsamples": nsamples, "shape_samples": shape_in,
+                                   "batch_size": hparams_dict["batch_size"]}}
 
         with open(js_file, "w") as jsf:
             js.dump(stat_info, jsf)
