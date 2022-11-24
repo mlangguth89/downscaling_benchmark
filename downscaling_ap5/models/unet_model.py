a6e87a3795f441046b2534785ea8da1277d6fae6
Michael <m.langguth@fz-juelich.de>
2022-11-24 13:09:17 +0100

    Fixes to model_utils.py and unet_model.py.

--- downscaling_ap5/models/unet_model.py
+++ downscaling_ap5/models/unet_model.py
@@ -9,13 +9,65 @@ Methods to set-up U-net models incl. its building blocks.
 
 # import modules
 import tensorflow as tf
-
+import tensorflow.keras as keras
 # all the layers used for U-net
 from tensorflow.keras.layers import (Concatenate, Conv2D, Conv2DTranspose, Input, MaxPool2D)
 from tensorflow.keras.models import Model
-from model_utils import conv_block, conv_block_n
 
 # building blocks for Unet
+
+
+def conv_block(inputs, num_filters: int, kernel: tuple = (3, 3), strides: tuple = (1, 1), padding: str = "same",
+               activation: str = "relu", activation_args=None, kernel_init: str = "he_normal",
+               l_batch_normalization: bool = True):
+    """
+    A convolutional layer with optional batch normalization
+    :param inputs: the input data with dimensions nx, ny and nc
+    :param num_filters: number of filters (output channel dimension)
+    :param kernel: tuple for convolution kernel size
+    :param strides: tuple for stride of convolution
+    :param padding: technique for padding (e.g. "same" or "valid")
+    :param activation: activation fuction for neurons (e.g. "relu")
+    :param activation_args: arguments for activation function given that advanced layers are applied
+    :param kernel_init: initialization technique (e.g. "he_normal" or "glorot_uniform")
+    :param l_batch_normalization: flag if batch normalization should be applied
+    """
+    x = Conv2D(num_filters, kernel, strides=strides, padding=padding, kernel_initializer=kernel_init)(inputs)
+    if l_batch_normalization:
+        x = BatchNormalization()(x)
+
+    try:
+        x = Activation(activation)(x)
+    except ValueError:
+        ac_layer = advanced_activation(activation, *activation_args)
+        x = ac_layer(x)
+
+    return x
+
+
+def conv_block_n(inputs, num_filters: int, n: int = 2, kernel: tuple = (3, 3), strides: tuple = (1, 1),
+                 padding: str = "same", activation: str = "relu", activation_args=None,
+                 kernel_init: str = "he_normal", l_batch_normalization: bool = True):
+    """
+    Sequential application of two convolutional layers (using conv_block).
+    :param inputs: the input data with dimensions nx, ny and nc
+    :param num_filters: number of filters (output channel dimension)
+    :param n: number of convolutional blocks
+    :param kernel: tuple for convolution kernel size
+    :param strides: tuple for stride of convolution
+    :param padding: technique for padding (e.g. "same" or "valid")
+    :param activation: activation fuction for neurons (e.g. "relu")
+    :param activation_args: arguments for activation function given that advanced layers are applied
+    :param kernel_init: initialization technique (e.g. "he_normal" or "glorot_uniform")
+    :param l_batch_normalization: flag if batch normalization should be applied
+    """
+    x = conv_block(inputs, num_filters, kernel, strides, padding, activation, activation_args,
+                   kernel_init, l_batch_normalization)
+    for _ in np.arange(n - 1):
+        x = conv_block(x, num_filters, kernel, strides, padding, activation, activation_args,
+                       kernel_init, l_batch_normalization)
+
+    return x
 
 
 def encoder_block(inputs, num_filters, kernel_maxpool: tuple = (2, 2), l_large: bool = True):
@@ -97,3 +149,10 @@ def get_lr_scheduler():
     lr_scheduler_cb = tf.keras.callbacks.LearningRateScheduler(lr_scheduler)
 
     return lr_scheduler_cb
+
+
+class UNET(keras.Model):
+    def __init__():
+        raise ValueError("Not implemented yet!")
+
+
