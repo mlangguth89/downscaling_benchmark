6961c7f3465f5d6cfefe4a48ba42b43030e70243
Michael <m.langguth@fz-juelich.de>
2022-12-08 18:05:06 +0100

    Several bugfixes in main_postprocess.py.

--- downscaling_ap5/main_scripts/main_postprocess.py
+++ downscaling_ap5/main_scripts/main_postprocess.py
@@ -25,11 +25,12 @@ from handle_data_class import HandleDataClass, get_dataset_filename
 from all_normalizations import ZScore
 from statistical_evaluation import Scores
 from postprocess import get_model_info, run_evaluation_time, run_evaluation_spatial
+from datetime import datetime as dt
 
 # get logger
 logger = logging.getLogger("main_postprocessing_logger")
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname): %(message)')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 
 
 def main(parser_args):
@@ -42,6 +43,7 @@ def main(parser_args):
                                                               parser_args.exp_name, parser_args.last,
                                                               parser_args.model_type)
     # create logger handlers
+    os.makedirs(plt_dir, exist_ok=True)
     fh = logging.FileHandler(os.path.join(plt_dir, f"postprocessing_{parser_args.exp_name}.log"))
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
@@ -50,8 +52,8 @@ def main(parser_args):
     ch.setFormatter(formatter)
 
     logger.addHandler(fh), logger.addHandler(ch)
-
-    logger.info(f"Start postprocessing at {t0}")
+    
+    logger.info(f"Start postprocessing at {dt.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
     # read configuration files
     md_config_pattern, ds_config_pattern = f"config_{model_type}.json", f"config_ds_{parser_args.dataset}.json"
@@ -61,8 +63,9 @@ def main(parser_args):
         raise FileNotFoundError(f"Could not find expected configuration file for dataset '{ds_config_pattern}' " +
                                 f"under '{model_dir}'")
     else:
-        with ds_config_file[0] as dsf:
-            logging.info(f"Read dataset configuration file '{ds_config_file[0]}'.")
+        print(ds_config_file)
+        with open(ds_config_file[0]) as dsf:
+            logger.info(f"Read dataset configuration file '{ds_config_file[0]}'.")
             ds_dict = js.load(dsf)
             logging.debug(ds_dict)
 
@@ -70,28 +73,28 @@ def main(parser_args):
         raise FileNotFoundError(f"Could not find expected configuration file for model '{md_config_pattern}' " +
                                 f"under '{model_dir}'")
     else:
-        with md_config_file[0] as mdf:
-            logging.info(f"Read model configuration file '{md_config_file[0]}'.")
+        with open(md_config_file[0]) as mdf:
+            logger.info(f"Read model configuration file '{md_config_file[0]}'.")
             hparams_dict = js.load(mdf)
             logging.debug(hparams_dict)
 
     # Load checkpointed model
-    logging.info(f"Load model '{parser_args.exp_name}' from {model_dir}")
+    logger.info(f"Load model '{parser_args.exp_name}' from {model_dir}")
     trained_model = keras.models.load_model(model_dir, compile=False)
-    logging.info(f"Model was loaded successfully.")
+    logger.info(f"Model was loaded successfully.")
 
     # get datafile and read netCDF
     fdata_test = get_dataset_filename(parser_args.data_dir, parser_args.dataset, "test",
                                       ds_dict.get("laugmented", False))
 
-    logging.info(f"Start opening datafile {fdata_test}...")
+    logger.info(f"Start opening datafile {fdata_test}...")
     ds_test = xr.open_dataset(fdata_test)
 
     # prepare training and validation data
-    logging.info(f"Start preparing test dataset...")
+    logger.info(f"Start preparing test dataset...")
     t0_preproc = timer()
 
-    da_test = HandleDataClass.reshape_ds(ds_test.astype("float32", copy=False)),
+    da_test = HandleDataClass.reshape_ds(ds_test.astype("float32", copy=False))
 
     # perform normalization
     js_norm = os.path.join(norm_dir, "norm.json")
@@ -105,17 +108,17 @@ def main(parser_args):
     logging.debug(f"Variable {tar_varname} serves as ground truth data.")
 
     if hparams_dict["z_branch"]:
-        logging.info(f"Add high-resolved target topography to input features.")
+        logger.info(f"Add high-resolved target topography to input features.")
         da_test_in = xr.concat([da_test_in, da_test_tar.isel({"variables": -1})], dim="variables")
 
     data_in = da_test_in.squeeze().values
 
     # start inference
-    logging.info(f"Preparation of test dataset finished after {timer() - t0_preproc}s." +
+    logger.info(f"Preparation of test dataset finished after {timer() - t0_preproc}s." +
                  "Start inference on trained model...")
     y_pred_trans = trained_model.predict(data_in, batch_size=32, verbose=2)
 
-    logging.info(f"Inference on test dataset finished. Start denormalization of output data...")
+    logger.info(f"Inference on test dataset finished. Start denormalization of output data...")
     # get coordinates and dimensions from target data
     coords = da_test_tar.isel(variables=0).squeeze().coords
     dims = da_test_tar.isel(variables=0).squeeze().dims
@@ -126,7 +129,7 @@ def main(parser_args):
     y_pred = xr.DataArray(y_pred, coords=coords, dims=dims)
 
     # start evaluation
-    logging.info(f"Output data on test dataset successfully processed in {t0 - timer()}s. Start evaluation...")
+    logger.info(f"Output data on test dataset successfully processed in {t0 - timer()}s. Start evaluation...")
 
     # create plot directory if required
     os.makedirs(plt_dir, exist_ok=True)
@@ -134,7 +137,7 @@ def main(parser_args):
     # instantiate score engine for time evaluation (i.e. hourly time series of evalutaion metrics)
     score_engine = Scores(y_pred, ground_truth, ds_dict["norm_dims"][1:])
 
-    logging.info("Start temporal evaluation...")
+    logger.info("Start temporal evaluation...")
     t0_tplot = timer()
     _ = run_evaluation_time(score_engine, "rmse", "K", plt_dir, value_range=(0., 3.), model_type=model_type)
     _ = run_evaluation_time(score_engine, "bias", "K", plt_dir, value_range=(-1., 1.), ref_line=0.,
@@ -142,12 +145,12 @@ def main(parser_args):
     _ = run_evaluation_time(score_engine, "grad_amplitude", "1", plt_dir, value_range=(0.7, 1.1),
                             ref_line=1., model_type=model_type)
 
-    logging.info(f"Temporal evalutaion finished in {timer() - t0_tplot}s.")
+    logger.info(f"Temporal evalutaion finished in {timer() - t0_tplot}s.")
 
     # instantiate score engine with retained spatial dimensions
     score_engine = Scores(y_pred, ground_truth, [])
 
-    logging.info("Start spatial evaluation...")
+    logger.info("Start spatial evaluation...")
     lvl_rmse = np.arange(0., 3.1, 0.2)
     cmap_rmse = mpl.cm.afmhot_r(np.linspace(0., 1., len(lvl_rmse)))
     _ = run_evaluation_spatial(score_engine, "rmse", "K", os.path.join(plt_dir, "rmse_spatial"), cmap=cmap_rmse,
@@ -158,9 +161,9 @@ def main(parser_args):
     _ = run_evaluation_spatial(score_engine, "bias", "K", os.path.join(plt_dir, "bias_spatial"), cmap=cmap_bias,
                                levels=lvl_bias)
 
-    logging.info(f"Temporal evalutaion finished in {timer() - t0_tplot}s.")
+    logger.info(f"Temporal evalutaion finished in {timer() - t0_tplot}s.")
 
-    logging.info(f"Postprocessing of experiment '{parser_args.exp_name}' finished." +
+    logger.info(f"Postprocessing of experiment '{parser_args.exp_name}' finished." +
                  "Elapsed total time: {t0 - timer()}s.")
 
 
@@ -168,6 +171,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_directory", "-data_dir", dest="data_dir", type=str, required=True,
                         help="Directory where test dataset (netCDF-file) is stored.")
+    parser.add_argument("--output_base_directory", "-output_base_dir", dest="output_base_dir", type=str, required=True,
+                        help="Directory where results in form of plots are stored.")
     parser.add_argument("--model_base_directory", "-model_base_dir", dest="model_base_dir", type=str, required=True,
                         help="Base directory where trained models are saved.")
     parser.add_argument("--experiment_name", "-exp_name", dest="exp_name", type=str, required=True,
@@ -176,7 +181,7 @@ if __name__ == "__main__":
                         help="Name of dataset to be used for downscaling model.")
     parser.add_argument("--evaluate_last", "-last", dest="last", default=False, action="store_true",
                         help="Flag for evaluating last instead of best checkpointed model")
-    parser.add_argument("--model_type", "-model_type", dest="model_tyoe", default=None,
+    parser.add_argument("--model_type", "-model_type", dest="model_type", default=None,
                         help="Name of model architecture. Only required if custom model architecture is not" +
                              "implemented in get_model_info-function (see postprocess.py)")
 
