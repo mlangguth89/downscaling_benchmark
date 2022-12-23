75a2fe9c7b2421a5882a52ff0ea6db1af6053226
maxim <maximbr@post.bgu.ac.il>
2022-12-23 14:08:55 +0200

    run script update

--- downscaling_SwinIR/HPC_scripts/run_postprocessing.sh
+++ downscaling_SwinIR/HPC_scripts/run_postprocessing.sh
@@ -32,7 +32,7 @@ source ../venv_booster/bin/activate
 datadir=/p/scratch/deepacf/maelstrom/maelstrom_data/ap5_michael/preprocessed_era5_crea6/netcdf_data/all_files/downscaling_tier2_train.nc
 model_basedir=../model_base_dir/
 outdir=../output/
-model_name = generator_step735000.pth
+model_name=generator_step735000.pth
 exp_name=wgan
 dataset=downscaling_tier2_train
 
