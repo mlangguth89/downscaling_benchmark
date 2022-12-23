0245e84104bc8d69e79b7b4449fa4ee6a9dbcbb4
maxim <maximbr@post.bgu.ac.il>
2022-12-23 14:16:50 +0200

    postprocessing

--- downscaling_SwinIR/HPC_scripts/run_postprocessing.sh
+++ downscaling_SwinIR/HPC_scripts/run_postprocessing.sh
@@ -37,7 +37,7 @@ exp_name=wgan
 dataset=downscaling_tier2_train
 
 # run job
-python ../main_scripts/main_postprocessing_pytorch.py -data_dir ${datadir} -model_base_dir ${model_basedir} \
+python ../main_scripts/main_postprocessing.py -data_dir ${datadir} -model_base_dir ${model_basedir} \
                                                                     -exp_name ${exp_name} -dataset ${dataset} -model_name ${model_name} \
                                                                     -output_base_directory ${outdir}
 
