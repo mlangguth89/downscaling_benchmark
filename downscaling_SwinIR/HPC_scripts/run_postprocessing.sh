7e1daefc19c6b0124e95de98dac3e60c597057b7
maxim <maximbr@post.bgu.ac.il>
2022-12-23 14:25:12 +0200

    postprocessing

--- downscaling_SwinIR/HPC_scripts/run_postprocessing.sh
+++ downscaling_SwinIR/HPC_scripts/run_postprocessing.sh
@@ -13,15 +13,10 @@
 #source ../venv_booster/bin/activate
 
 
-module purge
-module load Stages/2022 GCCcore/.11.2.0 GCC/11.2.0
-ml OpenMPI 4.1.2
-ml netCDF/4.8.1
-module load dask/2021.9.1
+module load Stages/2022 GCCcore/.11.2.0 dask/2021.9.1
 module load PyTorch/1.11-CUDA-11.5
 module load torchvision
-module load xarray/0.20.1
-
+#ml SciPy-bundle/2021.10
 source ../venv_booster/bin/activate
 
 
