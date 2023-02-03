7f0b33b9e0db5f49a725ceb487b40748f7f61043
Michael <m.langguth@fz-juelich.de>
2023-02-03 22:33:54 +0100

    Correction of effective batch-size bs_train in main_train_new.py.

--- downscaling_ap5/main_scripts/main_train_new.py
+++ downscaling_ap5/main_scripts/main_train_new.py
@@ -62,7 +62,7 @@ def main(parser_args):
     # Note: bs_train is introduced to allow substepping in the training loop, e.g. for WGAN where n optimization steps
     # are applied to train the critic, before the generator is trained once.
     # The validation dataset however does not perform substeeping and thus doesn't require an increased mini-batch size.
-    bs_train = ds_dict["batch_size"] * hparams_dict["d_steps"] + 1 if "d_steps" in hparams_dict else ds_dict["batch_size"]
+    bs_train = ds_dict["batch_size"] * (hparams_dict["d_steps"] + 1) if "d_steps" in hparams_dict else ds_dict["batch_size"]
 
     # start handling training and validation data
     #   - training data is iterated
