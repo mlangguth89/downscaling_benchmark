1eb362fe6a60bd1831be1e0deb841bcd2271a3ac
Michael <m.langguth@fz-juelich.de>
2023-02-07 08:01:07 +0100

    Update of test-scripts for data loading with xarray. These have also been shared with the support team.

--- downscaling_ap5/jupyter_notebooks_test/test_manual.py
+++ downscaling_ap5/jupyter_notebooks_test/test_manual.py
@@ -1,65 +1,59 @@
 import os, sys
 sys.path.append("../handle_data/")
+import argparse
 import glob
 import gc
 import psutil
+import multiprocessing
 from multiprocessing import Pool as ThreadPool
 import xarray as xr
 from functools import partial
 from all_normalizations import ZScore
-import random
 import time
 from timeit import default_timer as timer
 
 datadir = "/p/scratch/deepacf/maelstrom/maelstrom_data/ap5_michael/preprocessed_tier2/monthly_files/"
 file_list = glob.glob(os.path.join(datadir, "downscaling_tier2_train*.nc"))
-
+nfiles=30
 
 class Test:
     def __init__(self, file_list, data_norm, lparallel):
         self.file_list = file_list
         self.data_norm = data_norm
         self.data = None
-        self.samples_merged = 21348
         self.lparallel = lparallel
         if lparallel:
             print("Process will be parallelized.")
-            #self.pool = multiprocessing.pool.ThreadPool(multiprocessing.cpu_count())
+            # NOTE on assumed bug:
+            # Increasing thread pool to size of available CPUs results into a serious memory leak
             self.pool = ThreadPool(10)
+            #self.pool = ThreadPool(multiprocessing.cpu_count())
         else:
             print("Process will run sequentially.")
 
-    def _preprocess_ds(self, ds):
-        ds = self.data_norm.normalize(ds)
+    @staticmethod
+    def _preprocess_ds(ds, data_norm):
+        ds = data_norm.normalize(ds)
         return ds.astype("float32")
 
     @staticmethod
-    def _process_one_netcdf(fname):
+    def _process_one_netcdf(fname, data_norm):
         with xr.open_dataset(fname, decode_cf=False) as ds_now:
-            #ds_now = self._preprocess_ds(ds_now)
-            ds_now = ds_now.load()    
-        #    nsamples = ds_now.dims["time"]
+            ds_now = Test._preprocess_ds(ds_now, data_norm)
+            ds_now = ds_now.load()
             return ds_now
             
 
     def read_netcdf(self, ind_s, ind_e):
-        del self.data
-        gc.collect()
         file_list_now = self.file_list[ind_s:ind_e]
-        t0 = timer()
-        # read the normalized data into memory
-        def _process_one_netcdf(fname):
-            with xr.open_dataset(fname, decode_cf=False) as ds_now:
-                #ds_now = self._preprocess_ds(ds_now)
-                ds_now = ds_now.load()    
-                nsamples = ds_now.dims["time"]
-                return ds_now
-            
+        t0 = timer()           
         if self.lparallel:
-            datasets = self.pool.map(self._process_one_netcdf, file_list_now)
+            datasets = self.pool.map(partial(self._process_one_netcdf, data_norm=self.data_norm), file_list_now)
+            #datasets = self.pool.map(self._process_one_netcdf, file_list_now)
         else:
-            datasets = [_process_one_netcdf(p) for p in file_list_now]
+            datasets = [self._process_one_netcdf(p, self.data_norm) for p in file_list_now]
         self.data = xr.concat(datasets, dim="time")
+        nsamples = self.data.dims["time"]
         print(f"Reading datasets of files {ind_s} to {ind_e} took {timer()-t0:.2f}s")
         print(f"Size of loaded dataset: {self.data.nbytes/(1024**3):.2f} GB")
         del datasets
@@ -68,15 +62,19 @@ class Test:
 
         return True
 
-data_norm = ZScore(["time", "rlat", "rlon"])
-data_norm.read_norm_from_file("./norm_test.json")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lparallel", "-parallel", dest="parallel", default=False, action="store_true", help="Flag to parallelize reading data with multiprocessing.")
 
-test = Test(file_list, data_norm, lparallel=True)
+    args = parser.parse_args()
 
-print('RAM Used (GB):', psutil.virtual_memory()[3]/1000000000)
-test.read_netcdf(0, 30)
-print('RAM Used (GB):', psutil.virtual_memory()[3]/1000000000)
-test.read_netcdf(30, 60)
-print('RAM Used (GB):', psutil.virtual_memory()[3]/1000000000)
-test.read_netcdf(60, 90)
-print('RAM Used (GB):', psutil.virtual_memory()[3]/1000000000)
+    data_norm = ZScore(["time", "rlat", "rlon"])
+    data_norm.read_norm_from_file("./norm_test.json")
+
+    print('Start - RAM Used (GB):', psutil.virtual_memory()[3]/1024**3)
+    test = Test(file_list, data_norm, lparallel=args.parallel)
+    print('After init - RAM Used (GB):', psutil.virtual_memory()[3]/1024**3)
+
+    for i in range(3):
+        test.read_netcdf(i*nfiles, (i+1)*nfiles )
+        print(f"Read step {i} - RAM Used (GB): {psutil.virtual_memory()[3]/1024**3:.2f}")
--- downscaling_ap5/jupyter_notebooks_test/test_mfdataset.py
+++ downscaling_ap5/jupyter_notebooks_test/test_mfdataset.py
@@ -1,53 +1,65 @@
 import os, sys
 sys.path.append("../handle_data/")
 import glob
+import gc
+import psutil
+import argparse
 import xarray as xr
 from functools import partial
 from all_normalizations import ZScore
-import random
 import time
 from timeit import default_timer as timer
 
 datadir = "/p/scratch/deepacf/maelstrom/maelstrom_data/ap5_michael/preprocessed_tier2/monthly_files/"
 file_list = glob.glob(os.path.join(datadir, "downscaling_tier2_train*.nc"))
+nfiles=30
 
 class Test:
-    def __init__(self, file_list, data_norm):
+    def __init__(self, file_list, data_norm, lparallel=True):
         self.file_list = file_list
         self.data_norm = data_norm
         self.data = None
-        self.samples_merged = 21348
         self.lparallel = lparallel
-        if lparallel:
-            self.pool = multiprocessing.pool.ThreadPool(multiprocessing.cpu_count())
-
-    def _preprocess_ds(self, ds, data_norm):
-        ds = data_norm.normalize(ds)
+        
+    def _preprocess_ds(self, ds):
+        ds = self.data_norm.normalize(ds)
         return ds.astype("float32")
-
+    
+    def _process_netcdf(self, file_list):
+        with xr.open_mfdataset(list(file_list), decode_cf=False, preprocess=partial(self._preprocess_ds),
+                               parallel=self.lparallel, engine="h5netcdf") as ds_now:
+                return ds_now.load()
+            
     def read_netcdf(self, ind_s, ind_e):
+        del self.data
+        gc.collect()
         file_list_now = file_list[ind_s:ind_e]
         # read the normalized data into memory
         t0 = timer()
-        def _process_netcdf(file_list_now):
-            with xr.open_mfdataset(list(file_list_now), decode_cf=False, 
-                                       preprocess=partial(self._preprocess_ds, data_norm=self.data_norm), parallel=True) as ds_now:
-                ds_now = ds_now.load()    
-                nsamples = ds_now.dims["time"]
-                return ds_now
-        self.data = _process_netcdf(file_list_now)
+        self.data = self._process_netcdf(file_list_now)
+        nsamples = self.data.dims["time"]
         print(f"Reading datasets of files {ind_s} to {ind_e} took {timer()-t0:.2f}s")
         print(f"Size of loaded dataset: {self.data.nbytes/(1024**3):.2f} GB")
 
-        time.sleep(120)
+        gc.collect()
+        time.sleep(60)
 
         return True
 
-data_norm = ZScore(["time", "rlat", "rlon"])
-data_norm.read_norm_from_file("./norm_test.json")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lparallel", "-parallel", dest="parallel", default=False, action="store_true", help="Flag to parallelize reading data with multiprocessing.")
 
-test = Test(file_list, data_norm)
+    args = parser.parse_args()
 
-test.read_netcdf(0, 30)
-test.read_netcdf(30, 60)
-test.read_netcdf(60, 90)
+    data_norm = ZScore(["time", "rlat", "rlon"])
+    data_norm.read_norm_from_file("./norm_test.json")
+
+    print('Start - RAM Used (GB):', psutil.virtual_memory()[3]/1024**3)
+    test = Test(file_list, data_norm, lparallel=args.parallel)
+    print('After init - RAM Used (GB):', psutil.virtual_memory()[3]/1024**3)
+
+    for i in range(3):
+        test.read_netcdf(i*nfiles, (i+1)*nfiles )
+        print(f"Read step {i} - RAM Used (GB): {psutil.virtual_memory()[3]/1024**3:.2f}")
+
