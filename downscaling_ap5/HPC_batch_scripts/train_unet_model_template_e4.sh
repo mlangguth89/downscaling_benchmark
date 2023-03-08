eb02d8bdcfe0a2771fb2d207e5f1334e855b1e81
Michael Langguth <m.langguth@fz-juelich.de>
2023-03-08 17:40:46 +0100

    Differentiate between runscript templates for JSC cluster and e4.

---
+++ downscaling_ap5/HPC_batch_scripts/train_unet_model_template_e4.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -x
+#SBATCH --account=maelstrom
+#SBATCH --partition=i-gpu-a100
+##SBATCH --partition=a-gpu-mi100
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=128Gb
+#SBATCH --gres=gpu:1
+##SBATCH --mem=40G
+#SBATCH --time=01:00:00
+#SBATCH --output=train_wgan-model-out.%j
+#SBATCH --error=train_wgan-model-err.%j
+
+######### Template identifier (don't remove) #########
+echo "Do not run the template scripts"
+exit 99
+######### Template identifier (don't remove) #########
+
+# basic directories
+WORK_DIR=$(pwd)
+BASE_DIR=$(dirname "${WORK_DIR}")
+
+# Name of virtual environment
+VENV_DIR=/opt/share/users/maelstrom/
+VIRT_ENV_NAME=venv-rocm
+
+# Loading mouldes
+module purge
+ml slurm 
+
+# Activate virtual environment if needed (and possible)
+if [ -z ${VIRTUAL_ENV} ]; then
+   if [[ -f ${VENV_DIR}/${VIRT_ENV_NAME}/bin/activate ]]; then
+      echo "Activating virtual environment..."
+      source ${VENV_DIR}/${VIRT_ENV_NAME}/bin/activate
+   else
+      echo "ERROR: Requested virtual environment ${VIRT_ENV_NAME} not found..."
+      exit 1
+   fi
+fi
+
+export PYTHONPATH=${BASE_DIR}:$PYTHONPATH
+export PYTHONPATH=${BASE_DIR}/utils:$PYTHONPATH
+export PYTHONPATH=${BASE_DIR}/handle_data:$PYTHONPATH
+export PYTHONPATH=${BASE_DIR}/models:$PYTHONPATH
+export PYTHONPATH=${BASE_DIR}/postprocess:$PYTHONPATH
+echo ${PYTHONPATH}
+
+# data-directories 
+# Note template uses Tier2-dataset. Adapt accordingly for other datasets.
+indir=/data/maelstrom/langguth1/tier2/train
+outdir=<my_outdir>
+js_model_conf=${WORK_DIR}/config_unet.json
+js_ds_conf=${WORK_DIR}/config_ds_tier2.json
+
+model=unet
+dataset=tier2
+
+exp_name=<my_exp>
+
+# run job
+srun --overlap python3 ${BASE_DIR}/main_scripts/main_train.py -in ${indir} -out ${outdir} -model ${model} -dataset ${dataset} \
+	                                                           -conf_ds ${js_ds_conf} -conf_md ${js_model_conf} -exp_name ${exp_name} -id ${SLURM_JOBID}
+
