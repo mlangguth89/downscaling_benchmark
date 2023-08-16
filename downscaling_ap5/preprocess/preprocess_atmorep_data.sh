eaf97a49c1064bf9daa07bafde41dd59c2a1df49
Michael <m.langguth@fz-juelich.de>
2023-08-16 22:25:33 +0200

    Final fixes to preprocess_atmorep_data.sh.

--- downscaling_ap5/preprocess/preprocess_atmorep_data.sh
+++ downscaling_ap5/preprocess/preprocess_atmorep_data.sh
@@ -16,14 +16,15 @@
 
 era5_basedir=/p/scratch/atmo-rep/data/era5/ml_levels/
 crea6_basedir=/p/scratch/atmo-rep/data/cosmo_rea6/ml_levels/
-output_dir=/p/scratch/atmo-rep/data/era5/ml_levels/
+output_dir=/p/scratch/deepacf/maelstrom/maelstrom_data/ap5/competing_atmorep/
 crea6_gdes=../grid_des/crea6_reg_grid
 
 era5_vars=("t")
 era5_vars_full=("temperature")
 crea6_vars=("t2m")
+crea6_vars_full=("t_2m")
 
-ml_lvl_era5=( 96 105 114 123 135 )
+ml_lvl_era5=( 96 105 114 123 137 )
 ml_lvl_crea6=( 0 )
 
 year_start=1995
@@ -50,34 +51,47 @@ fi
 for yr in $(eval echo "{$year_start..$year_end}"); do 
     for mm in {01..12}; do
         echo "Processing ERA5 data for ${yr}-${mm}..."
-        for ivar in "${!era5_vars[@]}"; do 
-            for ml_lvl in ${ml_lvl_era5}; do 
-                # get file name
-                era5_file=${era5_basedir}/${ml_lvl}/"${era5_vars_full[ivar]}"/reanalysis_"${era5_vars_full[ivar]}"_y${yr}_m${mm}_ml${mlvl}.grib
+        for ivar in "${!era5_vars[@]}"; do
+	    echo "Processing variable ${era5_vars[ivar]}  from ERA5..."	
+            for ml_lvl in ${ml_lvl_era5[@]}; do 
+                echo "Processing data for level ${ml_lvl}"
+		# get file name
+                era5_file=${era5_basedir}/${ml_lvl}/"${era5_vars_full[ivar]}"/reanalysis_"${era5_vars_full[ivar]}"_y${yr}_m${mm}_ml${ml_lvl}.grib
                 tmp_file1=${tmp_dir}/era5_${era5_vars[ivar]}_y${yr}_m${mm}_ml${ml_lvl}_tmp1.nc 
                 tmp_file2=${tmp_dir}/era5_${era5_vars[ivar]}_y${yr}_m${mm}_ml${ml_lvl}_tmp2.nc
                 tmp_era5=${tmp_dir}/era5_${era5_vars[ivar]}_y${yr}_m${mm}_ml${ml_lvl}_tmp3.nc
                 # convert to netCDF and slice to region of interest
                 cdo -f nc copy -sellonlatbox,-1.5,25.75,42.25,56 ${era5_file} ${tmp_file1}
-                cdo remapbil,./crea6_reg_grid ${tmp_file1} ${tmp_file2}
+                cdo remapbil,${crea6_gdes} ${tmp_file1} ${tmp_file2}
                 # rename variable
                 cdo --reduce_dim chname,${era5_vars[ivar]},${era5_vars[ivar]}_ml${ml_lvl}_in -selvar,${era5_vars[ivar]} ${tmp_file2} ${tmp_era5} 
                 # clean-up
                 rm ${tmp_file1} ${tmp_file2}
-                break
             done
         done
+
+	era5_file_now=${tmp_dir}/era5_y${yr}_m${mm}.nc
+        cdo merge ${tmp_dir}/*.nc ${era5_file_now}
+	# clean-up
+	rm ${tmp_dir}/era5*tmp*.nc
 
         echo "Processing COSMO-REA6 data for ${yr}-${mm}..."
-        for crea6_var in ${crea6_vars[@]}; do 
+        for ivar in ${!crea6_vars[@]}; do 
             for ml_lvl in ${ml_lvl_crea6}; do
-                crea6_file=${crea6_basedir}/${ml_lvl}/${crea6_var}/cosmo_rea6_${crea6_var}_y${yr}_m${mm}_ml${mlvl}.nc
-                tmp_crea6=${tmp_dir}/crea6_${crea6_var}_y${yr}_m${mm}_ml${ml_lvl}_tmp.nc
-                cdo --reduce_dim -chname,${crea6_var},${crea6_var}_ml${ml_lvl}_tar -sellonlatbox,-1.25,25.6875,42.3125,55.75 -selvar,${crea6_var} ${crea6_file} ${tmp_crea6}
+                crea6_file=${crea6_basedir}/${ml_lvl}/${crea6_vars[ivar]}/cosmo_rea6_${crea6_vars[ivar]}_y${yr}_m${mm}_ml${ml_lvl}.nc
+                tmp_crea6=${tmp_dir}/crea6_${crea6_vars[ivar]}_y${yr}_m${mm}_ml${ml_lvl}_tmp_crea6.nc
+                cdo --reduce_dim -chname,${crea6_vars_full[ivar]},${crea6_vars[ivar]}_ml${ml_lvl}_tar -sellonlatbox,-1.25,25.6875,42.3125,55.75 -selvar,${crea6_vars_full[ivar]} ${crea6_file} ${tmp_crea6}
             done
         done
 
-        cdo merge ${tmp_era5} ${tmp_crea6} ${output_dir}/downscaling_atmorep_train_${yr}-${mm}.nc;
+	crea6_file_now=${tmp_dir}/crea6_y${yr}_m${mm}.nc
+        cdo merge ${tmp_dir}/*tmp_crea6.nc ${crea6_file_now}
+
+        cdo merge ${crea6_file_now} ${era5_file_now} ${output_dir}/downscaling_atmorep_train_${yr}-${mm}.nc;
+	
+	# clean-up
+	rm ${tmp_dir}/*.nc
+	exit
     done
 done
 
