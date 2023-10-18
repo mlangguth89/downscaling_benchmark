7c46d17bf733dc12324631feb81187994ad6a4ed
Michael <m.langguth@fz-juelich.de>
2023-10-18 07:55:51 +0200

    Adaptions to enable running tuned U-Net configuration.

--- downscaling_ap5/models/unet_model.py
+++ downscaling_ap5/models/unet_model.py
@@ -9,17 +9,18 @@ Methods to set-up U-net models incl. its building blocks.
 __author__ = "Michael Langguth"
 __email__ = "m.langguth@fz-juelich.de"
 __date__ = "2021-XX-XX"
-__update__ = "2023-05-06"
+__update__ = "2023-10-17"
 
 # import modules
 import os
 from typing import List
+import inspect
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
 # all the layers used for U-net
 from tensorflow.keras.layers import (Concatenate, Conv2D, Conv2DTranspose, Input, MaxPool2D, BatchNormalization,
-                                     Activation)
+                                     Activation, AveragePooling2D)
 from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import LearningRateScheduler, ModelCheckpoint, EarlyStopping
 
@@ -82,7 +83,7 @@ def conv_block_n(inputs, num_filters: int, n: int = 2, kernel: tuple = (3, 3), s
     return x
 
 
-def encoder_block(inputs, num_filters, kernel_maxpool: tuple = (2, 2), l_large: bool = True):
+def encoder_block(inputs, num_filters, kernel_pool: tuple = (2, 2), l_large: bool = True, activation: str = "relu", l_avgpool: bool = False):
     """
     One complete encoder-block used in U-net.
     :param inputs: input to encoder block
@@ -91,22 +92,41 @@ def encoder_block(inputs, num_filters, kernel_maxpool: tuple = (2, 2), l_large: 
     :param l_large: flag for large encoder block (two consecutive convolutional blocks)
     """
     if l_large:
-        x = conv_block_n(inputs, num_filters, n=2)
+        x = conv_block_n(inputs, num_filters, n=2, activation=activation)
     else:
-        x = conv_block(inputs, num_filters)
+        x = conv_block(inputs, num_filters, activation=activation)
 
-    p = MaxPool2D(kernel_maxpool)(x)
+    if l_avgpool:
+        p = AveragePooling2D(kernel_pool)(x)
+    else:
+        p = MaxPool2D(kernel_pool)(x)
 
     return x, p
+
+def subpixel_block(inputs, num_filters, kernel: tuple = (3,3), upscale_fac: int = 2,
+                   padding: str = "same", activation: str = "relu", kernel_init: str = "he_normal"):
+
+    x = Conv2D(num_filters * (upscale_fac ** 2), kernel, padding=padding, kernel_initializer=kernel_init,
+               activation=activation)(inputs)
+    x = tf.nn.depth_to_space(x, upscale_fac)
+
+    return x
+
 
 
 def decoder_block(inputs, skip_features, num_filters, kernel: tuple = (3, 3), strides_up: int = 2,
                   padding: str = "same", activation="relu", kernel_init="he_normal",
-                  l_batch_normalization: bool = True):
+                  l_batch_normalization: bool = True, l_subpixel: bool = False):
     """
     One complete decoder block used in U-net (reverting the encoder)
     """
-    x = Conv2DTranspose(num_filters, (strides_up, strides_up), strides=strides_up, padding="same")(inputs)
+    if l_subpixel:
+        x = Conv2DTranspose(num_filters, (strides_up, strides_up), strides=strides_up, padding="same", 
+                            activation=activation, kernel_init=kernel_init)(inputs)
+    else:
+        x = subpixel_block(inputs, num_filters, kernel, upscale_fac=strides_up, padding=padding,
+                            activation=activation, kernel_init=kernel_init)
+        
     x = Concatenate()([x, skip_features])
     x = conv_block_n(x, num_filters, 2, kernel, (1, 1), padding, activation, kernel_init=kernel_init, 
                      l_batch_normalization=l_batch_normalization)
@@ -116,31 +136,43 @@ def decoder_block(inputs, skip_features, num_filters, kernel: tuple = (3, 3), st
 
 # The particular U-net
 def sha_unet(input_shape: tuple, n_predictands_dyn: int, channels_start: int = 56, z_branch: bool = False,
-             concat_out: bool = False, tar_channels=["output_dyn", "output_z"]) -> Model:
+             advanced_unet: bool = False, concat_out: bool = False, tar_channels=["output_dyn", "output_z"]) -> Model:
     """
     Builds up U-net model architecture adapted from Sha et al., 2020 (see https://doi.org/10.1175/JAMC-D-20-0057.1).
     :param input_shape: shape of input-data
     :param channels_start: number of channels to use as start in encoder blocks
     :param n_predictands: number of target variables (dynamic output variables)
     :param z_branch: flag if z-branch is used.
-    :param tar_channels: name of output/target channels (needed for associating losses during compilation)
+    :param advanced_unet: flag if advanced U-net is used (LeakyReLU instead of ReLU, average pooling instead of max pooling and subpixel-layer)
     :param concat_out: boolean if output layers will be concatenated (disables named target channels!)
+    :param tar_channels: name of output/target channels (needed for associating losses during compilation)
     :return:
     """
     inputs = Input(input_shape)
 
+    if advanced_unet:
+        config_encoder = {"activation": "leakyrelu"}
+        config_decoder = config_encoder.copy()
+        config_encoder["l_avgpool"] = True
+        config_decoder["l_subpixel"] = True
+    else:   
+        config_encoder = {"activation": "relu"}
+        config_decoder = config_encoder.copy()
+        config_encoder["l_avgpool"] = False
+        config_decoder["l_subpixel"] = False
+
     """ encoder """
-    s1, e1 = encoder_block(inputs, channels_start, l_large=True)
-    s2, e2 = encoder_block(e1, channels_start * 2, l_large=False)
-    s3, e3 = encoder_block(e2, channels_start * 4, l_large=False)
+    s1, e1 = encoder_block(inputs, channels_start, l_large=True, **config_encoder)
+    s2, e2 = encoder_block(e1, channels_start * 2, l_large=False, **config_encoder)
+    s3, e3 = encoder_block(e2, channels_start * 4, l_large=False, **config_encoder)
 
     """ bridge encoder <-> decoder """
-    b1 = conv_block(e3, channels_start * 8)
+    b1 = conv_block(e3, channels_start * 8, activation=config_encoder["activation"])
 
     """ decoder """
-    d1 = decoder_block(b1, s3, channels_start * 4)
-    d2 = decoder_block(d1, s2, channels_start * 2)
-    d3 = decoder_block(d2, s1, channels_start)
+    d1 = decoder_block(b1, s3, channels_start * 4, **config_decoder)
+    d2 = decoder_block(d1, s2, channels_start * 2,**config_decoder)
+    d3 = decoder_block(d2, s1, channels_start, **config_decoder)
 
     output_dyn = Conv2D(n_predictands_dyn, (1, 1), kernel_initializer="he_normal", name=tar_channels[0])(d3)
     if z_branch:
@@ -210,13 +242,16 @@ class UNET(keras.Model):
         return opt_dict
 
     def compile(self, **kwargs):
+
+        add_unet_args = {"advanced_unet": self.hparams["advanced_unet"]} if "advanced_unet" in inspect.getargspec(self.unet).args else {}
+
         # instantiate model
         if self.hparams["z_branch"]:     # model has named branches (see also opt_dict in get_compile_opts)
             tar_channels = [f"{var}" for var in self.varnames_tar]
             self.unet = self.unet(self.shape_in, self.n_predictands_dyn, z_branch=True,
-                                  concat_out= False, tar_channels=tar_channels)
+                                  concat_out=False, tar_channels=tar_channels, **add_unet_args)
         else:
-            self.unet = self.unet(self.shape_in, self.n_predictands_dyn, z_branch=False)
+            self.unet = self.unet(self.shape_in, self.n_predictands_dyn, z_branch=False, **add_unet_args)
 
         return self.unet.compile(**kwargs)
        # return super(UNET, self).compile(**kwargs)
@@ -314,7 +349,8 @@ class UNET(keras.Model):
         """
         hparams_dict = {"batch_size": 32, "lr": 5.e-05, "nepochs": 70, "z_branch": True, "loss_func": "mae",
                         "loss_weights": [1.0, 1.0], "lr_decay": False, "decay_start": 5, "decay_end": 30,
-                        "lr_end": 1.e-06, "l_embed": False, "ngf": 56, "optimizer": "adam", "lscheduled_train": True}
+                        "lr_end": 1.e-06, "l_embed": False, "ngf": 56, "optimizer": "adam", "lscheduled_train": True,
+                        "advanced_unet": False}
 
         return hparams_dict
 
