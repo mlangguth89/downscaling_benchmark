605f7ce95b1e44c391381b8679700840f21f0e03
Michael <m.langguth@fz-juelich.de>
2023-10-31 11:32:44 +0100

    Final versions of main_postprocess_destineaq.py  incl. bug fix in convert_testds_to_hourly.ipynb.

--- downscaling_ap5/main_scripts/main_postprocess_destineaq.py
+++ downscaling_ap5/main_scripts/main_postprocess_destineaq.py
@@ -16,7 +16,6 @@ import logging
 import argparse
 from timeit import default_timer as timer
 import json as js
-from datetime import datetime as dt
 import gc
 import pandas as pd
 import xarray as xr
@@ -29,15 +28,20 @@ import matplotlib as mpl
 from handle_data_unet import *
 from handle_data_class import HandleDataClass
 from all_normalizations import ZScore
+from model_utils import ModelEngine
 from plotting import create_map_score
 from postprocess import get_model_info 
 from model_utils import convert_to_xarray
+from datetime import datetime as dt
 
 # get logger
 logger = logging.getLogger(os.path.basename(__file__).rstrip(".py"))
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 
+
+def errorhandler(exc):
+    print('Exception:', exc)
 
 def main(parser_args):
 
@@ -93,7 +97,7 @@ def main(parser_args):
     named_targets = hparams_dict.get("named_targets", False)
 
     # get datafile list
-    fdata_test_list = glob.glob(os.path.join(parser_args.data_dir, "downscaling_{parser_args.dataset}*.nc"))
+    fdata_test_list = glob.glob(os.path.join(parser_args.data_dir, f"downscaling_{parser_args.dataset}*.nc"))
     nfiles = len(fdata_test_list)
     if nfiles == 0:
         raise FileNotFoundError(f"Could not find any datafile for dataset '{parser_args.dataset}' " +
@@ -111,7 +115,7 @@ def main(parser_args):
     # initialize lists for timing
     times_read, times_preproc, times_infer, times_postproc, times_plot = [], [], [], [], []
 
-    for fdata_test in fdata_test_list:
+    for fdata_test in fdata_test_list[10:]:
         # Read test data
         t0_read = timer()
         logger.info(f"Start reading data from file '{fdata_test}'")
@@ -131,15 +135,16 @@ def main(parser_args):
         da_test = HandleDataClass.reshape_ds(ds_test).astype("float32", copy=True)
         
         # get date of test data and create directory
-        date_str = pd.to_datetime(da_test["time"][0].values).strftime("%Y%m%d_%H%M")
+        date_str = pd.to_datetime(da_test["time"][0].values).strftime("%Y%m%d")
         plt_dir_date = os.path.join(plt_dir, date_str)
+        os.makedirs(plt_dir_date, exist_ok=True)
     
         # clean-up to reduce memory footprint
         del ds_test
         gc.collect()
 
         # instantiate tf.data.Dataset object
-        tfds_opts = {"batch_size": ds_dict["batch_size"], "predictands": ds_dict["predictands"], "predictors": ds_dict.get("predictors", None),
+        tfds_opts = {"batch_size": 24, "predictands": ds_dict["predictands"], "predictors": ds_dict.get("predictors", None),
                     "lshuffle": False, "var_tar2in": ds_dict["var_tar2in"], "named_targets": named_targets, "lrepeat": False, "drop_remainder": False}    
 
         tfds_test = HandleDataClass.make_tf_dataset_allmem(da_test, **tfds_opts)
@@ -160,7 +165,16 @@ def main(parser_args):
         t0_mload = timer()
 
         logger.info(f"Load model '{parser_args.exp_name}' from {model_dir}")
-        trained_model = keras.models.load_model(model_dir, compile=False)
+        #trained_model = keras.models.load_model(model_dir, compile=False)
+        
+        # initialize model with new input size
+        shape_in = tfds_test.element_spec[0].shape
+        wgan_instance = ModelEngine("wgan")
+        wgan = wgan_instance(shape_in[1:], ds_dict["predictands"], hparams_dict, model_dir, parser_args.exp_name)
+        trained_model = wgan.generator
+        trained_model.build(input_shape=shape_in)
+        # load weights
+        trained_model.load_weights(os.path.join(model_dir, "generator.h5"))
 
         dt_mload = timer() - t0_mload
         logger.info(f"Loading model took {dt_mload:.2f}s.")
@@ -182,10 +196,10 @@ def main(parser_args):
         # Start postprocessing
         logger.info(f"Start postprocessing of inference data...")
         t0_postproc = timer()
-    
+   
         # convert to xarray
-        y_pred = convert_to_xarray(y_pred, norm, tar_varname, da_test.sel({"variables": tar_varname}).squeeze().coords,
-                                   da_test.sel({"variables": tar_varname}).squeeze().dims, hparams_dict["z_branch"])
+        y_pred = convert_to_xarray(y_pred - 273.15, norm, tar_varname, da_test.sel({"variables": tar_varname}).squeeze().coords,
+                                   ["time", "rlat", "rlon"], hparams_dict["z_branch"])
 
         # write inference data to netCDf
         ncfile_out = os.path.join(plt_dir_date, f"{model_type}_downscaled_{tar_varname}_{date_str}.nc")
@@ -206,19 +220,19 @@ def main(parser_args):
         # set plot arguments
         proj=ccrs.RotatedPole(pole_longitude=-162.0, pole_latitude=39.25)
         lvl_t2m = np.arange(-34, 46, 2.)
-        cmap_t2m = mpl.hsv_r(np.linspace(0., 1., len(lvl_t2m)))
+        cmap_t2m = mpl.cm.hsv_r(np.linspace(0., 1., len(lvl_t2m)))
         plt_kwargs = {"dims": ds_dict["norm_dims"][1::], "cmap": cmap_t2m, "levels": lvl_t2m,
-                      "projection": proj, "title": ""}
+                      "projection": proj, "title": "", "extent": [3.5, 20., 44.5, 57.] }
         
         # create a pool of processes
         nworkers = min(mp.cpu_count(), len(y_pred["time"]))
         pool = Pool(processes=nworkers)
 
         for it, t in enumerate(y_pred["time"]):
-            date = pd.to_datetime(t)
+            date = pd.to_datetime(t.values)
             fname = os.path.join(plt_dir_date, f"{model_type}_downscaled_{tar_varname}_{date.strftime('%Y%m%d_%H00')}")
             plt_kwargs["title"] = f"{date.strftime('%Y-%m-%d %H:00 UTC')}"
-            pool.apply_async(create_map_score, (y_pred.isel({"time": it}), fname), plt_kwargs)
+            pool.apply_async(create_map_score, (y_pred.isel({"time": it}), fname), plt_kwargs, error_callback=errorhandler)
             
         pool.close()
         pool.join()
@@ -227,6 +241,9 @@ def main(parser_args):
         dt_plot = timer() - t0_plot
         times_plot.append(dt_plot)
         logger.info(f"Creatimg maps of downscaled product took {dt_plot:.2f}s.")
+
+        # clear TF session to ensure that model is built up from scratch
+        keras.backend.clear_session()
 
     # finalize postprocessing and save tracked times to JSON-file
     dt_all = timer() - t0
