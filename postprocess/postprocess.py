e87de6b9a5a581f3c1d3a88049a6091ecf2d9973
Michael <m.langguth@fz-juelich.de>
2024-01-28 16:50:37 +0100

    Add run_spectral_analysis-method to postprocess.py.

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -17,8 +17,8 @@ import logging
 import numpy as np
 import xarray as xr
 from cartopy import crs
-from statistical_evaluation import feature_importance
-from plotting import create_line_plot, create_map_score, create_box_plot
+from statistical_evaluation import feature_importance, get_spectrum
+from plotting import create_line_plot, create_map_score, create_box_plot, create_ps_plot
 
 # basic data types
 da_or_ds = Union[xr.DataArray, xr.Dataset]
@@ -27,7 +27,6 @@ list_or_str = Union[List[str], str]
 # auxiliary variable for logger
 logger_module_name = f"main_postprocess.{__name__}"
 module_logger = logging.getLogger(logger_module_name)
-
 
 def get_model_info(model_base, output_base: str, exp_name: str, bool_last: bool = False, model_type: str = None):
 
@@ -59,6 +58,7 @@ def get_model_info(model_base, output_base: str, exp_name: str, bool_last: bool 
         model_type = model_type
 
     return model_dir, plt_dir, norm_dir, model_type
+
 
 
 def run_feature_importance(ds: xr.DataArray, predictors: list_or_str, varname_tar: str, model, norm, score_name: str,
@@ -187,6 +187,44 @@ def run_evaluation_spatial(score_engine, score_name: str, plot_dir: str,
                              title=f"{score_name} {sea.values} {hh:02d} UTC", **plt_kwargs)
 
     return True
+
+def run_spectral_analysis(ds: xr.Dataset, plt_dir: str, varname: str, var_unit: str,
+                          lonlat_dims: list_or_str = ["rlon", "rlat"], lcutoff: bool= True, re: float = 6371.):
+    """
+    Run spectral analysis for all experiments provided in xr.Dataset and plot the data.
+    """
+    func_logger = logging.getLogger(f"{logger_module_name}.{run_spectral_analysis.__name__}")
+
+    exps = list(ds.data_vars)
+    nexps = len(exps)
+
+    ps_list = []
+
+    # compute wave numbers based on size of input data
+    nlon, nlat = ds[lonlat_dims[0]].size, ds[lonlat_dims[1]].size
+
+    dims = ["wavenumber"]
+    coord_dict = {"wavenumber": np.arange(0, np.amin(np.array([int(nlon/2), int(nlat/2)])))}
+
+    for i, exp in enumerate(exps):
+        func_logger.info(f"Start spectral analysis for experiment {exp} ({i+1}/{nexps})...")
+
+        # run spectral analysis
+        ps_exp = get_spectrum(ds[exp], lonlat_dims = lonlat_dims, lcutoff= lcutoff, re=re)
+        # average over all time steps and create xarray.DataArray
+        da_ps_exp = xr.DataArray(ps_exp.mean(axis=0), dims=dims, coords=coord_dict, name=exp)
+
+        # remove wavenumber 0 and append to list    
+        ps_list.append(da_ps_exp[1::])
+
+    # create xarray.Dataset 
+    ds_ps = xr.Dataset(ps_list)
+
+    # create plot   
+    plt_fname = os.path.join(plt_dir, f"{varname}_power_spectrum.png")
+    create_ps_plot(ds_ps, {varname: f"{var_unit}**2 m"}, plt_fname, colors= ["navy", "green"],
+                   x_coord="wavenumber")
+    
 
 
 def scores_to_csv(score_mean, score_std, score_name, fname="scores.csv"):
