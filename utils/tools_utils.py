a930f5c723700d4dd81d481fa6329e571081f81b
Michael <m.langguth@fz-juelich.de>
2025-02-17 15:56:07 +0100

    Updated license headers in all utils-scripts.

--- utils/tools_utils.py
+++ utils/tools_utils.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Earth System Data Exploration (ESDE), Jülich Supercomputing Center (JSC); Gesosphere Austria (GSA)
+#
+# SPDX-License-Identifier: MIT
+
 __author__ = "Michael Langguth"
 __email__ = "m.langguth@fz-juelich.de"
 __date__ = "2022-03-14"
