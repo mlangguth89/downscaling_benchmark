531351dfd6a24b632a2526b935d518affd58d809
Michael <m.langguth@fz-juelich.de>
2025-02-17 15:59:10 +0100

    Updated license headers in all postprocess-scripts.

--- postprocess/plotting.py
+++ postprocess/plotting.py
@@ -1,7 +1,6 @@
-# SPDX-FileCopyrightText: 2024 Earth System Data Exploration (ESDE), Jülich Supercomputing Center (JSC)
+# SPDX-FileCopyrightText: 2025 Earth System Data Exploration (ESDE), Jülich Supercomputing Center (JSC); Gesosphere Austria (GSA)
 #
 # SPDX-License-Identifier: MIT
-
 """
 Methods for creating plots.
 """
