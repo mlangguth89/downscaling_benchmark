abbdb952d3b712b1cef0e8819991b0f962c32e36
seblehner <sebastian.lehner@geosphere.at>
2025-04-23 17:54:38 +0200

    MNT: move default fss configs into eval dict defaults for temporal eval

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -775,21 +775,21 @@ class TemporalEvaluation(AbstractMetricEvaluation):
                          "grad_amplitude": {"score_unit": "1", "value_range": (0.7, 1.1), "ref_line": 1.},
                          "me_std": {"score_unit": "K", "value_range": (0.1, 0.3), "ref_line": None},
                          "ralsd": {"score_unit": "dB", "value_range": (0., 5.), "ref_line": None},
-                         "fss": {"score_unit": "1", "value_range": (0, 1.), "ref_line": 0.5},
+                         "fss": {"score_unit": "1", "value_range": (0, 1.), "ref_line": 0.5, "window": (144, 128), "thres": 310},
                         }
         elif self.varname == "wind":
             eval_dict = {"rmse": {"score_unit": "m/s", "value_range": (0., 3.), "ref_line": None, "relative": False}, 
                          "bias": {"score_unit": "m/s", "value_range": (-1., 1.), "ref_line": 0, "relative": False},
                          "grad_amplitude": {"score_unit": "1", "value_range": (0.7, 1.1), "ref_line": 1.},
                          "me_std": {"score_unit": "m/s", "value_range": (0.1, 0.3), "ref_line": None},
-                         "fss": {"score_unit": "1", "value_range": (0, 1.), "ref_line": 0.5},
+                         "fss": {"score_unit": "1", "value_range": (0, 1.), "ref_line": 0.5, "window": (144, 128), "thres": 10},
                         }
         elif self.varname == "irradiance":
             eval_dict = {"rmse": {"score_unit": "W/m^2", "value_range": (0., 3.), "ref_line": None, "relative": False}, 
                          "bias": {"score_unit": "W/m^2", "value_range": (-1., 1.), "ref_line": 0, "relative": False},
                          "grad_amplitude": {"score_unit": "1", "value_range": (0.7, 1.1), "ref_line": 1.},
                          "me_std": {"score_unit": "W/m^2", "value_range": (0.1, 0.3), "ref_line": None},
-                         "fss": {"score_unit": "1", "value_range": (0, 1.), "ref_line": 0.5},
+                         "fss": {"score_unit": "1", "value_range": (0, 1.), "ref_line": 0.5, "window": (144, 128), "thres": 500},
                         }
         else:
             if eval_dict is None:
