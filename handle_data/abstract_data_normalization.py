5ba6595d30ef07893aa177efe0feba4a0e4fd8e1
Michael <m.langguth@fz-juelich.de>
2025-05-16 08:24:05 +0200

    Fix issues with handling data-independant normalization parameters.

--- handle_data/abstract_data_normalization.py
+++ handle_data/abstract_data_normalization.py
@@ -66,7 +66,10 @@ class Normalize(ABC):
         #_ = self._check_norm_dims(data)
         # do the computation
         norm_stats = self.get_required_stats(data, **stats)
-        norm_stats = Normalize.match_datatype(data, *norm_stats)
+        if norm_stats is None:     # normalizing method does not have data-dependant parameters
+            norm_stats = ()
+        else:
+            norm_stats = Normalize.match_datatype(data, *norm_stats)
         data_denorm = self.denormalize_data(data, *norm_stats)
 
         return data_denorm
