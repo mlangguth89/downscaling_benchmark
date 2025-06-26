13741db54bd0ce1ef496668f80cd7bb41a707751
seblehner <sebastian.lehner@geosphere.at>
2025-06-26 14:14:12 +0200

    DEV: make plot_dir var name consistent in run functions

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -438,12 +438,12 @@ def run_evaluation_spatial(score_engine, score_name: str, plot_dir: str,
 
     return True
 
-def run_cond_quantile_analysis(data_fcst, data_ref, plt_dir, varname_lables, unit, **opts: dict):
+def run_cond_quantile_analysis(data_fcst, data_ref, plot_dir, varname_lables, unit, **opts: dict):
     """
     Create conditional quantile plots for given variables.
     :param data_fcst: xarray.DataArray with forecast data
     :param data_ref: xarray.DataArray with reference data
-    :param plt_dir: Directory to save plot files
+    :param plot_dir: Directory to save plot files
     :param varname_lables: List of variable names
     :param unit: Unit of variable
     :param opt: Dictionary with configuration options
@@ -466,7 +466,7 @@ def run_cond_quantile_analysis(data_fcst, data_ref, plt_dir, varname_lables, uni
     quantile_panel_all, marginal_all = calculate_cond_quantiles(data_fcst, data_ref, varname_lables, unit, factorization=factorization, quantiles=quantiles)
 
     # create plot
-    plt_fname = os.path.join(plt_dir, f"conditional_quantile_plot_{factorization}_all.png")
+    plt_fname = os.path.join(plot_dir, f"conditional_quantile_plot_{factorization}_all.png")
     plot_cond_quantile(quantile_panel_all, marginal_all, plt_fname, **opts)
 
     # conditional quantile analysis for each season
@@ -478,16 +478,16 @@ def run_cond_quantile_analysis(data_fcst, data_ref, plt_dir, varname_lables, uni
 
         quantile_panel_sea, marginal_sea = calculate_cond_quantiles(data_fcst_sea, data_ref_sea, varname_lables, unit, factorization=factorization, quantiles=quantiles)
 
-        plt_fname = os.path.join(plt_dir, f"conditional_quantile_plot_{factorization}_{sea}.png")
+        plt_fname = os.path.join(plot_dir, f"conditional_quantile_plot_{factorization}_{sea}.png")
         plot_cond_quantile(quantile_panel_sea, marginal_sea, plt_fname, **opts) 
 
-def run_marginal_analysis(data_fcst: xr.DataArray, data_ref: xr.DataArray, plt_dir: str, labels: List[str], varname: str, unit: str, **opts: dict):
+def run_marginal_analysis(data_fcst: xr.DataArray, data_ref: xr.DataArray, plot_dir: str, labels: List[str], varname: str, unit: str, **opts: dict):
     """
     Perform marginal analysis for forecast (downscaled) and reference data
     which includes calculation of the Interquartile Distance (IQD) score and plotting both histograms.
     :param data_fcst: xarray.DataArray with forecast (downscaled) data
     :param data_ref: xarray.DataArray with reference (ground truth) data
-    :param plt_dir: Directory to save plot files
+    :param plot_dir: Directory to save plot files
     :param labels: List of labels for forecast and reference data
     :param varname: Physical name of variable
     :param unit: Physical unit of variable
@@ -505,10 +505,10 @@ def run_marginal_analysis(data_fcst: xr.DataArray, data_ref: xr.DataArray, plt_d
     func_logger.info(f"IQD for all {varname} data: {iqd: .2e}")
 
     # create output-directories if necessary
-    os.makedirs(plt_dir, exist_ok=True)
+    os.makedirs(plot_dir, exist_ok=True)
 
     # create histogram plot for all data
-    plt_fname = os.path.join(plt_dir, f"histogram_{varname}_all.png")
+    plt_fname = os.path.join(plot_dir, f"histogram_{varname}_all.png")
     plot_histograms(data_fcst, data_ref, plt_fname, labels, iqd, xlabel=f"{varname} [{unit}]", **opts)
 
     # conditional quantile analysis for each season
@@ -523,11 +523,11 @@ def run_marginal_analysis(data_fcst: xr.DataArray, data_ref: xr.DataArray, plt_d
 
         func_logger.info(f"IQD for {varname} data from season {sea}: {iqd_sea: .2e}")
 
-        plt_fname = os.path.join(plt_dir, f"histogram_{varname}_{sea}.png")
+        plt_fname = os.path.join(plot_dir, f"histogram_{varname}_{sea}.png")
         plot_histograms(data_fcst_sea, data_ref_sea, plt_fname, labels, iqd_sea, xlabel=f"{varname} [{unit}]", **opts)
                            
 
-def run_spectral_analysis(ds: xr.Dataset, data_vars: List[str], plt_dir: str, labels: List[str], varname: str, var_unit: str,
+def run_spectral_analysis(ds: xr.Dataset, data_vars: List[str], plot_dir: str, labels: List[str], varname: str, var_unit: str,
                           lonlat_dims: list_or_str = ["rlon", "rlat"], lcutoff: bool= True, re: float = 6371., **plt_kwargs):
     """
     Run spectral analysis, create power spectrum plot and save results into a netCDF-file.
@@ -536,7 +536,7 @@ def run_spectral_analysis(ds: xr.Dataset, data_vars: List[str], plt_dir: str, la
     Example: Spectral analysis for 2m temperature from downscaling and reference (ground truth) data.
     :param ds: xarray.Dataset with input data
     :param data_vars: List of variable names from ds for spectral analysis 
-    :param plt_dir: Directory to save plot files
+    :param plot_dir: Directory to save plot files
     :param labels: List of labels for each variable
     :param varname: Physical name of quantity
     :param var_unit: Physical unit of quantity
@@ -547,7 +547,7 @@ def run_spectral_analysis(ds: xr.Dataset, data_vars: List[str], plt_dir: str, la
     """
     func_logger = logging.getLogger(f"{logger_module_name}.{run_spectral_analysis.__name__}")
     
-    metric_dir = plt_dir.replace("/plots/", "/metric_files/")
+    metric_dir = plot_dir.replace("/plots/", "/metric_files/")
     os.makedirs(metric_dir, exist_ok=True)
 
     # check if number of vairables for spectral analysis and labels are equal
@@ -571,10 +571,10 @@ def run_spectral_analysis(ds: xr.Dataset, data_vars: List[str], plt_dir: str, la
     ds_ps = get_spectrum_exps(ds, ds_vars, info, lcutoff=lcutoff, re=re) 
 
     # create plot
-    os.makedirs(plt_dir, exist_ok=True)
+    os.makedirs(plot_dir, exist_ok=True)
     colors = plt_kwargs.pop("colors", ["navy", "green"])
 
-    plt_fname = os.path.join(plt_dir, f"{varname}_power_spectrum_all.png")
+    plt_fname = os.path.join(plot_dir, f"{varname}_power_spectrum_all.png")
     plot_power_spectra(ds_ps, {varname: var_unit_spec}, labels, plt_fname, colors= colors,
                        x_coord="wavenumber", **plt_kwargs)
     
@@ -592,7 +592,7 @@ def run_spectral_analysis(ds: xr.Dataset, data_vars: List[str], plt_dir: str, la
 
         ds_ps_sea = get_spectrum_exps(ds_sea, ds_vars, info, lcutoff=lcutoff, re=re)
 
-        plt_fname = os.path.join(plt_dir, f"{varname}_power_spectrum_{sea}.png")
+        plt_fname = os.path.join(plot_dir, f"{varname}_power_spectrum_{sea}.png")
         plot_power_spectra(ds_ps_sea, {varname: var_unit_spec}, labels, plt_fname, colors= colors,
                            x_coord="wavenumber", **plt_kwargs)
         
@@ -604,7 +604,7 @@ def run_spectral_analysis(ds: xr.Dataset, data_vars: List[str], plt_dir: str, la
         
 
 def run_feature_importance(ds: xr.Dataset, predictors: list_or_str, varname_tar: str, model, norm, score_name: str,
-                           data_loader_opt: dict, plt_dir: str, patch_size = (6, 6)):
+                           data_loader_opt: dict, plot_dir: str, patch_size = (6, 6)):
     """
     Run feature importance analysis and create box-plot of results
     :param ds: Unnormalized xr.Dataset with predictors and target variable
@@ -614,7 +614,7 @@ def run_feature_importance(ds: xr.Dataset, predictors: list_or_str, varname_tar:
     :param norm: Normalization object
     :param score_name: Name of score to compute feature importance
     :param data_loader_opt: Data loader options that will be parsed to the make_tf_dataset_allmem-method
-    :param plt_dir: Directory to save plot files
+    :param plot_dir: Directory to save plot files
     :param patch_size: Patch size for feature importance analysis
     """
     # get local logger
@@ -627,7 +627,7 @@ def run_feature_importance(ds: xr.Dataset, predictors: list_or_str, varname_tar:
     
     # get reference score
     func_logger.debug(f"Retrieve reference score to finish feature importance analysis...")
-    score_file = os.path.join(plt_dir.replace("/plots/", "/metric_files/"), f"eval_{score_name}_year.nc")
+    score_file = os.path.join(plot_dir.replace("/plots/", "/metric_files/"), f"eval_{score_name}_year.nc")
     if not os.path.exists(score_file):
         raise FileNotFoundError(f"File {score_file} not found. Run run_evaluation_time-method for score '{score_name}' first.")
     ds_score = xr.open_dataset(score_file)
@@ -637,7 +637,7 @@ def run_feature_importance(ds: xr.Dataset, predictors: list_or_str, varname_tar:
     max_rel_change = int(np.ceil(np.amax(rel_changes) + 1.))
 
     # plot feature importance scores in a box-plot with whiskers where each variable is a box
-    plt_fname = os.path.join(plt_dir, f"feature_importance_{score_name}.png")
+    plt_fname = os.path.join(plot_dir, f"feature_importance_{score_name}.png")
 
     func_logger.debug(f"Plot feature importance-analysis results into file '{plt_fname}'.")
     create_box_plot(rel_changes.T, plt_fname, **{"title": f"Feature Importance ({score_name.upper()})", "ref_line": 1., "widths": .3, 
@@ -714,14 +714,14 @@ def feature_importance(ds: xr.Dataset, predictors: list_or_str, varname_tar: str
 
     return score_all
 
-def run_comparison_plots(ds, plt_dir, score_name, model_type, nsamples = 200, offset = 0., seasonal_levels: bool = True, **kwargs):
+def run_comparison_plots(ds, plot_dir, score_name, model_type, nsamples = 200, offset = 0., seasonal_levels: bool = True, **kwargs):
     """
     Run comparison plots for a given number of samples. The samples will be picked based on the performance 
     of the downscaling model in terms of the provided score.
     For instance, for nsamples=100 and score_name='rmse', samples with an RMSE corresponding to
     the 0th, 1st, 2nd, ... 100th percentile will be selected.
     :param ds: xarray.Dataset providing the downscaled and reference/ground truth data
-    :param plt_dir: Directory to save plot files
+    :param plot_dir: Directory to save plot files
     :param score_name: Name of score to determine which samples to plot
     :param model_type: Type of model
     :param nsamples: Number of samples to plot
@@ -733,10 +733,10 @@ def run_comparison_plots(ds, plt_dir, score_name, model_type, nsamples = 200, of
     func_logger = logging.getLogger(f"{logger_module_name}.{run_comparison_plots.__name__}")
 
     # create output-directories if necessary
-    os.makedirs(plt_dir, exist_ok=True)
+    os.makedirs(plot_dir, exist_ok=True)
 
     # get score data
-    metric_dir = plt_dir.replace("/plots/", "/metric_files/").replace("/comparison_plots", "/temporal_evaluation")
+    metric_dir = plot_dir.replace("/plots/", "/metric_files/").replace("/comparison_plots", "/temporal_evaluation")
     score_file = os.path.join(metric_dir, f"eval_{score_name}_year.nc")
     if not os.path.exists(score_file):
         raise FileNotFoundError(f"File {score_file} not found. Run run_evaluation_time-method for score '{score_name}' first.")
@@ -764,7 +764,7 @@ def run_comparison_plots(ds, plt_dir, score_name, model_type, nsamples = 200, of
         kwargs_now = kwargs.copy()
         date_str = (pd.to_datetime(t.values)).strftime("%Y%m%dT%H00")
         quantile_now = f"{(i+1) / nsamples:.3f}".replace(".", "p")
-        fname = os.path.join(plt_dir, f"{model_type}_{varname}_{date_str}_{score_name}_q{quantile_now}.png")
+        fname = os.path.join(plot_dir, f"{model_type}_{varname}_{date_str}_{score_name}_q{quantile_now}.png")
         if varname == "t2m" and seasonal_levels:
            kwargs_now["levels"] = get_season_t2m_levels(t.values)
         pool.apply_async(plot_comparison_maps, (ds.sel({"time": t}) + offset, fname), kwargs_now, error_callback=errorhandler)
