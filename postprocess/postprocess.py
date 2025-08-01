e5abe3a68a44e337c020b15ba57777e48f0e0d58
seblehner <sebastian.lehner@geosphere.at>
2025-08-01 18:31:10 +0200

    DEV: expand glob_rad metrics to include both absolute and relative by default for rmse and bias

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -887,8 +887,10 @@ class TemporalEvaluation(AbstractMetricEvaluation):
                          "me_std": {"score_unit": "m/s", "value_range": (0.1, 0.3), "ref_line": None},
                         }
         elif self.varname == "glob_rad":
-            eval_dict = {"rmse": {"score_unit": "W/m^2", "value_range": (0., 3.), "ref_line": None, "relative": True}, 
-                         "bias": {"score_unit": "W/m^2", "value_range": (-1., 1.), "ref_line": 0, "relative": True},
+            eval_dict = {"rmse": {"score_unit": "W/m^2", "value_range": (0., 3.), "ref_line": None, "relative": False}, 
+                         "bias": {"score_unit": "W/m^2", "value_range": (-1., 1.), "ref_line": 0, "relative": False},
+                         "rmse_relative": {"score_unit": "W/m^2", "value_range": (0., 3.), "ref_line": None, "relative": True}, 
+                         "bias_relative": {"score_unit": "W/m^2", "value_range": (-1., 1.), "ref_line": 0, "relative": True},
                          "grad_amplitude": {"score_unit": "1", "value_range": (0.7, 1.2), "ref_line": 1.},
                          "me_std": {"score_unit": "W/m^2", "value_range": (0.1, 0.3), "ref_line": None},
                          "fss": {"score_unit": "1", "value_range": (0, 1.), "ref_line": 0.5, "window": (4, 4), "thres": [50, 100, 300, 500]},
@@ -938,8 +940,11 @@ class SpatialEvaluation(AbstractMetricEvaluation):
             eval_dict = {"rmse": {"score_unit": "m/s", "levels": lvl_rmse, "cmap_name": "afmhot_r", "relative": False}, 
                          "bias": {"score_unit": "m/s", "levels": lvl_bias, "cmap_name": "seismic", "relative": False}}
         elif self.varname == "glob_rad":
-            eval_dict = {"rmse": {"score_unit": "W/m^2", "levels": lvl_rmse, "cmap_name": "afmhot_r", "relative": True}, 
-                         "bias": {"score_unit": "W/m^2", "levels": lvl_bias, "cmap_name": "seismic", "relative": True}}
+            eval_dict = {"rmse": {"score_unit": "W/m^2", "levels": lvl_rmse, "cmap_name": "afmhot_r", "relative": False}, 
+                         "bias": {"score_unit": "W/m^2", "levels": lvl_bias, "cmap_name": "seismic", "relative": False},
+                         "rmse_relative": {"score_unit": "W/m^2", "levels": lvl_rmse, "cmap_name": "afmhot_r", "relative": True}, 
+                         "bias_relative": {"score_unit": "W/m^2", "levels": lvl_bias, "cmap_name": "seismic", "relative": True}
+                        }
         else:
             if eval_dict is None:
                 raise ValueError(f"No default configuration available for variable {self.varname}. " + \
