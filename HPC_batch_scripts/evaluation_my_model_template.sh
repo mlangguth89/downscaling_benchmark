7841cd23ce41e88e7513c13d834dac1c175d09ed
seblehner <sebastian.lehner@geosphere.at>
2025-10-03 11:20:48 +0200

    RFC: rename postprocess template batch script to evaluation

--- HPC_batch_scripts/evaluation_my_model_template.sh
+++ HPC_batch_scripts/evaluation_my_model_template.sh
@@ -26,7 +26,7 @@ VENV_DIR=${BASE_DIR}/virtual_envs/
 VIRT_ENV_NAME=<my_venv>
 
 # Loading mouldes
-source ../env_setup/modules_jsc.sh
+source ../env_setup/modules_jsc_evaluation.sh
 # Activate virtual environment if needed (and possible)
 if [ -z ${VIRTUAL_ENV} ]; then
    if [[ -f ${VENV_DIR}/${VIRT_ENV_NAME}/bin/activate ]]; then
